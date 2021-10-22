f92fd601cd88246b14de8e6a5915663dd40e0679
Sandeep Nagra <san@initialspark.co.uk>
2021-10-22 14:01:24 +0100

    Add gov uk design system js file

--- src/assets/javascript/application.js
+++ src/assets/javascript/application.js
@@ -1,6 +1,8 @@
 (function (w) {
   "use strict";
   function appInit(trackingId, analyticsCookieDomain) {
+    window.GOVUKFrontend.initAll();
+    
     var cookies = window.GOVSignIn.Cookies(trackingId, analyticsCookieDomain);
 
     if (cookies.hasConsentForAnalytics()) {
