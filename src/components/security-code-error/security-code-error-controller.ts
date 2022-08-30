9b4605dd723041a4412133d68ef10294ea488961
ayoshebby <ayo.sebiotimo@digital.cabinet-office.gov.uk>
2022-08-30 15:18:04 +0100

    AUT-607: Use /request-new-opt-code url for newCodeLink when OTP limit exceeded when changing phone number

--- src/components/security-code-error/security-code-error-controller.ts
+++ src/components/security-code-error/security-code-error-controller.ts
@@ -3,6 +3,6 @@ import { PATH_DATA } from "../../app.constants";
 
 export function securityCodeInvalidGet(req: Request, res: Response): void {
   res.render("security-code-error/index.njk", {
-    newCodeLink: PATH_DATA.RESEND_MFA_CODE,
+    newCodeLink: PATH_DATA.REQUEST_NEW_CODE_OTP.url,
   });
 }
