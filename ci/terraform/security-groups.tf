ad360708cddbc2da0fc64611ebf6a99eb33ff220
domiliauskas <domiliauskas@deloitte.co.uk>
2022-10-06 12:13:43 +0100

    ATB-10 Working version some refactoring required

--- ci/terraform/security-groups.tf
+++ ci/terraform/security-groups.tf
@@ -1,46 +1,46 @@
-resource "aws_security_group" "am_frontend_redis_security_group" {
-  name_prefix = "${var.environment}-am-frontend-redis-security-group-"
-  description = "Allow ingress to AM frontend Redis. Use on Elasticache cluster only"
-  vpc_id      = local.vpc_id
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = local.default_tags
-}
-
-resource "aws_security_group_rule" "allow_incoming_am_frontend_redis_from_private_subnet" {
-  security_group_id = aws_security_group.am_frontend_redis_security_group.id
-
-  from_port   = local.redis_port_number
-  protocol    = "tcp"
-  cidr_blocks = local.private_subnet_cidr_blocks
-  to_port     = local.redis_port_number
-  type        = "ingress"
-}
-
-resource "aws_security_group" "allow_access_to_am_frontend_redis" {
-  name_prefix = "${var.environment}-allow-access-to-acct-mgmt-frontend-redis-"
-  description = "Allow outgoing access to the Account Management frontend Redis session store"
-  vpc_id      = local.vpc_id
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = local.default_tags
-}
-
-resource "aws_security_group_rule" "allow_connection_to_am_frontend_redis" {
-  security_group_id = aws_security_group.allow_access_to_am_frontend_redis.id
-
-  from_port                = local.redis_port_number
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.am_frontend_redis_security_group.id
-  to_port                  = local.redis_port_number
-  type                     = "egress"
-}
+//resource "aws_security_group" "am_frontend_redis_security_group" {
+//  name_prefix = "${var.environment}-am-frontend-redis-security-group-"
+//  description = "Allow ingress to AM frontend Redis. Use on Elasticache cluster only"
+//  vpc_id      = local.vpc_id
+//
+//  lifecycle {
+//    create_before_destroy = true
+//  }
+//
+//  tags = local.default_tags
+//}
+//
+//resource "aws_security_group_rule" "allow_incoming_am_frontend_redis_from_private_subnet" {
+//  security_group_id = aws_security_group.am_frontend_redis_security_group.id
+//
+//  from_port   = local.redis_port_number
+//  protocol    = "tcp"
+//  cidr_blocks = local.private_subnet_cidr_blocks
+//  to_port     = local.redis_port_number
+//  type        = "ingress"
+//}
+//
+//resource "aws_security_group" "allow_access_to_am_frontend_redis" {
+//  name_prefix = "${var.environment}-allow-access-to-acct-mgmt-frontend-redis-"
+//  description = "Allow outgoing access to the Account Management frontend Redis session store"
+//  vpc_id      = local.vpc_id
+//
+//  lifecycle {
+//    create_before_destroy = true
+//  }
+//
+//  tags = local.default_tags
+//}
+//
+//resource "aws_security_group_rule" "allow_connection_to_am_frontend_redis" {
+//  security_group_id = aws_security_group.allow_access_to_am_frontend_redis.id
+//
+//  from_port                = local.redis_port_number
+//  protocol                 = "tcp"
+//  source_security_group_id = aws_security_group.am_frontend_redis_security_group.id
+//  to_port                  = local.redis_port_number
+//  type                     = "egress"
+//}
 
 resource "aws_security_group" "account_management_alb_sg" {
   name_prefix = "${var.environment}-account-management-alb-"
--- deploy/image/app.py
+++ deploy/image/app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def hello():
+#     print("This is a log message")
+#     return "Hello World!"
 
+import time
+import redis
+import socket
+import requests
+from flask import Flask
+import os
 app = Flask(__name__)
+cache = redis.Redis(host=os.getenv('REDIS_URL','redis://:MVrPokyz45imdiBeMyuBzNCAECpI92Sk@clustercfg.shared-redis-cache-cluster.gnivhg.euw2.cache.amazonaws.com'), port=6379)
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            print("Exception occured while connecting redis")
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.route("/")
 def hello():
     print("This is a log message")
     return "Hello World!"
+
+@app.route('/redis')
+def hit():
+    count = get_hit_count()
+    return "<html>Welcome to Redis Test App - Home Page - on node %s.<br \>Hit count = %i.<br \></html>" % (socket.gethostname(), int(count))
