8d2f351614d65e185db1b31fddfb0bf7df5b9fb1
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-12-08 17:25:57 +0000

    GUA-470: Existing tests cover Delete with feature flag off
    Let's keep these tests to cover the "When the feature Flag is off"
    Scenario. Also add an expection to ensure we DO NOT call the new
    functionality along the way.

--- src/components/delete-account/tests/delete-account-controller.test.ts
+++ src/components/delete-account/tests/delete-account-controller.test.ts
@@ -58,51 +58,120 @@ describe("delete account controller", () => {
   });
 
   describe("deleteAccountPost", () => {
-    it("should redirect to deletion confirmed page", async () => {
-      req = validRequest();
-      const fakeService: DeleteAccountServiceInterface = {
-        deleteAccount: sandbox.fake(),
-      };
+    describe("when supporting DELETE_SERVICE_STORE", () => {
+      beforeEach(() => {
+        process.env.SUPPORT_DELETE_SERVICE_STORE = "1"
+      });
+    
+      afterEach(() => {
+        delete process.env.SUPPORT_DELETE_SERVICE_STORE;
+      });
+      it("should redirect to deletion confirmed page", async () => {
+        req = validRequest();
+        const fakeService: DeleteAccountServiceInterface = {
+          deleteAccount: sandbox.fake(),
+          deleteServiceData: sandbox.fake(),
+        };
 
-      const fakePublishingService: GovUkPublishingServiceInterface = {
-        notifyAccountDeleted: sandbox.fake.returns(Promise.resolve()),
-        notifyEmailChanged: sandbox.fake(),
-      };
+        const fakePublishingService: GovUkPublishingServiceInterface = {
+          notifyAccountDeleted: sandbox.fake.returns(Promise.resolve()),
+          notifyEmailChanged: sandbox.fake(),
+        };
 
-      req.session.user.email = "test@test.com";
-      req.session.user.subjectId = "public-subject-id";
-      req.session.user.tokens = { accessToken: "token" };
-      req.oidc = {
-        endSessionUrl: sandbox.fake.returns("logout-url"),
-      };
+        req.session.user.email = "test@test.com";
+        req.session.user.subjectId = "public-subject-id";
+        req.session.user.tokens = { accessToken: "token" };
+        req.oidc = {
+          endSessionUrl: sandbox.fake.returns("logout-url"),
+        };
 
-      await deleteAccountPost(fakeService, fakePublishingService)(
-        req as Request,
-        res as Response
-      );
+        await deleteAccountPost(fakeService, fakePublishingService)(
+          req as Request,
+          res as Response
+        );
 
-      expect(fakeService.deleteAccount).to.have.been.calledOnce;
-      expect(fakePublishingService.notifyAccountDeleted).to.have.been
-        .calledOnce;
-      expect(req.oidc.endSessionUrl).to.have.been.calledOnce;
-      expect(res.redirect).to.have.been.calledWith("logout-url");
-      expect(
-        req.app.locals.sessionStore.destroy.getCall(0).calledWith("session-1")
-      ).eq(true);
-      expect(
-        req.app.locals.sessionStore.destroy.getCall(1).calledWith("session-2")
-      ).eq(true);
+        expect(fakeService.deleteAccount).to.have.been.calledOnce;
+        expect(fakeService.deleteServiceData).to.have.been.calledOnce;
+        expect(fakePublishingService.notifyAccountDeleted).to.have.been
+          .calledOnce;
+        expect(req.oidc.endSessionUrl).to.have.been.calledOnce;
+        expect(res.redirect).to.have.been.calledWith("logout-url");
+        expect(
+          req.app.locals.sessionStore.destroy.getCall(0).calledWith("session-1")
+        ).eq(true);
+        expect(
+          req.app.locals.sessionStore.destroy.getCall(1).calledWith("session-2")
+        ).eq(true);
 
-      expect(
-        req.app.locals.subjectSessionIndexService.removeSession
-          .getCall(0)
-          .calledWith("public-subject-id", "session-1")
-      ).eq(true);
-      expect(
-        req.app.locals.subjectSessionIndexService.removeSession
-          .getCall(1)
-          .calledWith("public-subject-id", "session-2")
-      ).eq(true);
+        expect(
+          req.app.locals.subjectSessionIndexService.removeSession
+            .getCall(0)
+            .calledWith("public-subject-id", "session-1")
+        ).eq(true);
+        expect(
+          req.app.locals.subjectSessionIndexService.removeSession
+            .getCall(1)
+            .calledWith("public-subject-id", "session-2")
+        ).eq(true);
+      });
+    });
+
+    describe("when not supporting DELETE_SERVICE_STORE", () => {
+      beforeEach(() => {
+        process.env.SUPPORT_DELETE_SERVICE_STORE = "0"
+      });
+    
+      afterEach(() => {
+        delete process.env.SUPPORT_DELETE_SERVICE_STORE;
+      });
+      it("should redirect to deletion confirmed page", async () => {
+        req = validRequest();
+        const fakeService: DeleteAccountServiceInterface = {
+          deleteAccount: sandbox.fake(),
+          deleteServiceData: sandbox.fake(),
+        };
+
+        const fakePublishingService: GovUkPublishingServiceInterface = {
+          notifyAccountDeleted: sandbox.fake.returns(Promise.resolve()),
+          notifyEmailChanged: sandbox.fake(),
+        };
+
+        req.session.user.email = "test@test.com";
+        req.session.user.subjectId = "public-subject-id";
+        req.session.user.tokens = { accessToken: "token" };
+        req.oidc = {
+          endSessionUrl: sandbox.fake.returns("logout-url"),
+        };
+
+        await deleteAccountPost(fakeService, fakePublishingService)(
+          req as Request,
+          res as Response
+        );
+
+        expect(fakeService.deleteAccount).to.have.been.called;
+        expect(fakeService.deleteServiceData).not.to.have.been.calledOnce;
+        expect(fakePublishingService.notifyAccountDeleted).to.have.been
+          .calledOnce;
+        expect(req.oidc.endSessionUrl).to.have.been.calledOnce;
+        expect(res.redirect).to.have.been.calledWith("logout-url");
+        expect(
+          req.app.locals.sessionStore.destroy.getCall(0).calledWith("session-1")
+        ).eq(true);
+        expect(
+          req.app.locals.sessionStore.destroy.getCall(1).calledWith("session-2")
+        ).eq(true);
+
+        expect(
+          req.app.locals.subjectSessionIndexService.removeSession
+            .getCall(0)
+            .calledWith("public-subject-id", "session-1")
+        ).eq(true);
+        expect(
+          req.app.locals.subjectSessionIndexService.removeSession
+            .getCall(1)
+            .calledWith("public-subject-id", "session-2")
+        ).eq(true);
+      });
     });
   });
 });
