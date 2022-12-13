6ca25f3565e1facde1c12381407576b91a415067
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-12-13 17:16:06 +0000

    GUA-462: Get AppEnv and use that to call the correct Env Client Reg
    For the moment we want to maintain these separately, mostly because
    different envs will have different client IDs, so let's pass the env
    down through the controller and vary which value we're calling out of
    the env.
    
    Co-authored-by: Divya Parameswaran <divya.parameswaran@digital.cabinet-office.gov.uk>
    Co-authored-by: Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>

--- src/components/your-services/your-services-controller.ts
+++ src/components/your-services/your-services-controller.ts
@@ -1,21 +1,24 @@
 import { Request, Response } from "express";
 import { presentYourServices } from "../../utils/yourServices";
+import { getAppEnv } from "../../config";
 
 export async function yourServicesGet(
   req: Request,
   res: Response
 ): Promise<void> {
   const { user } = req.session;
+  const env = getAppEnv();
   if (user && user.subjectId) {
     const serviceData = await presentYourServices(user.subjectId);
     const data = {
       email: req.session.user.email,
       accountsList: serviceData.accountsList,
       servicesList: serviceData.servicesList,
+      env: env,
     };
 
     res.render("your-services/index.njk", data);
   } else {
-    res.render("your-services/index.njk", { email: user.email });
+    res.render("your-services/index.njk", { email: user.email, env: env });
   }
 }
