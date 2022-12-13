2b8edcc184f271024679af970918b15482987430
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-12-13 17:17:00 +0000

    GUA-462: Add test suite for your-services-controller
    Co-authored-by: Divya Parameswaran <divya.parameswaran@digital.cabinet-office.gov.uk>
    Co-authored-by: Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>

---
+++ src/components/your-services/tests/your-services-controller.test.ts
@@ -0,0 +1,74 @@
+import { expect } from "chai";
+import { describe } from "mocha";
+
+import { sinon } from "../../../../test/utils/test-utils";
+import { Request, Response } from "express";
+
+import { yourServicesGet } from "../your-services-controller";
+import { getAppEnv } from "../../../config";
+describe("your services controller", () => {
+  let sandbox: sinon.SinonSandbox;
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  const TEST_SUBJECT_ID = "testSubjectId";
+  function validRequest(): any {
+    return {
+      app: {
+        locals: {
+          sessionStore: {
+            destroy: sandbox.fake(),
+          },
+          subjectSessionIndexService: {
+            removeSession: sandbox.fake(),
+            getSessions: sandbox.stub().resolves(["session-1", "session-2"]),
+          },
+        },
+      },
+      body: {},
+      session: {
+        user: { subjectId: TEST_SUBJECT_ID, email: "test@test.com" },
+        destroy: sandbox.fake(),
+      },
+      log: { error: sandbox.fake() },
+    };
+  }
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+
+    res = { render: sandbox.fake(), redirect: sandbox.fake(), locals: {} };
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe("yourServicesGet", () => {
+    it("should render your services page with data", async () => {
+      req = validRequest();
+      await yourServicesGet(req as Request, res as Response);
+      expect(res.render).to.have.calledWith("your-services/index.njk", {
+        email: "test@test.com",
+        accountsList: [],
+        servicesList: [],
+        env: getAppEnv(),
+      });
+    });
+
+    it("should render your services page with email ", async () => {
+      const req: any = {
+        body: {},
+        session: {
+          user: { email: "test@test.com" },
+          destroy: sandbox.fake(),
+        },
+      };
+
+      await yourServicesGet(req as Request, res as Response);
+      expect(res.render).to.have.calledWith("your-services/index.njk", {
+        email: "test@test.com",
+        env: getAppEnv(),
+      });
+    });
+  });
+});
