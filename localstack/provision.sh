310c1f6e351ac5efeb1891d5804d50a9e7e9c794
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-12-13 18:02:14 +0000

    GUA-462: Support pretty dates in provisioning script
    We've added a new field with a pre-formatted data. Let's add that to the
    provisioning script so we can have that rendered locally
    
    Co-authored-by: Divya Parameswaran <divya.parameswaran@digital.cabinet-office.gov.uk>
    Co-authored-by: Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>

--- localstack/provision.sh
+++ localstack/provision.sh
@@ -26,7 +26,7 @@ aws --endpoint-url=http://localhost:4566 dynamodb put-item \
     --region eu-west-2 \
     --profile $AWS_PROFILE \
     --item \
-        "{\"user_id\":{\"S\": \"$BUILD_CLIENT_ID\"}, \"services\": {\"L\":[{\"M\":{\"count_successful_logins\":{\"N\":\"2\"},\"client_id\":{\"S\":\"client_id_1\"},\"last_accessed\":{\"N\":\"1666169856\"}}}]}}"
+        "{\"user_id\":{\"S\": \"$BUILD_CLIENT_ID\"}, \"services\": {\"L\":[{\"M\":{\"count_successful_logins\":{\"N\":\"2\"},\"client_id\":{\"S\":\"gov-uk\"},\"last_accessed\":{\"N\":\"1666169856\"},\"last_accessed_pretty\":{\"S\":\"20 January 1970\"}}}]}}"
 
 aws --endpoint-url=http://localhost:4566 dynamodb get-item \
     --table-name $TABLE_NAME  \
