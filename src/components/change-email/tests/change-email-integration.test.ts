f5aa752f22f2d306ea85ccb217657ce062f122d7
Chiara <cpascucci@deloitte.co.uk>
2023-01-31 11:34:41 +0000

    reverting

--- src/components/change-email/tests/change-email-integration.test.ts
+++ src/components/change-email/tests/change-email-integration.test.ts
@@ -72,9 +72,6 @@ describe("Integration:: change email", () => {
         token = $("[name=_csrf]").val();
         cookies = res.headers["set-cookie"];
       });
-
-    // eslint-disable-next-line no-console
-    console.log(token);
   });
 
   beforeEach(() => {
