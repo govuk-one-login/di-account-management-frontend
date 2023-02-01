683361517da76a1ebff5f3cbb3ff702c4a44c4a8
Ana Hickmann <ana.hickmann@digital.cabinet-office.gov.uk>
2023-02-01 13:44:34 +0000

    GUA-595: Added log to check what's being published to SNS

--- src/components/delete-account/delete-account-service.ts
+++ src/components/delete-account/delete-account-service.ts
@@ -2,6 +2,7 @@ import { snsService } from "../../utils/sns";
 import { SnsService } from "../../utils/types";
 import { getSNSDeleteTopic } from "../../config";
 import { DeleteAccountServiceInterface } from "./types";
+import {logger} from "../../utils/logger";
 
 export function deleteAccountService(
     sns: SnsService = snsService()
@@ -17,6 +18,15 @@ export function deleteAccountService(
         legacy_subject_id: string,
         topic_arn: string = getSNSDeleteTopic(),
     ): Promise<void> {
+        logger.info(`Message to publish to SNS: ${ JSON.stringify({"user_id": user_id,
+        "access_token": access_token,
+        "email": email,
+        "source_ip": source_ip,
+        "session_id": session_id,
+        "persistent_session_id": persistent_session_id,
+        "public_subject_id": public_subject_id,
+        "legacy_subject_id": legacy_subject_id})}`)
+        
         await sns.publish(topic_arn, JSON.stringify({
             "user_id": user_id,
             "access_token": access_token,
