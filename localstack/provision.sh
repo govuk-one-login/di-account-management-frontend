8ec5c63bd5b4f6a5cba04becc8d278475e02566c
Divya <divya.parameswaran@digital.cabinet-office.gov.uk>
2023-06-22 11:05:10 +0100

    GUA-856 : Add the activity log dynamo reference

--- localstack/provision.sh
+++ localstack/provision.sh
@@ -6,6 +6,7 @@
 # or what ever the hardcoded replacement is
 export BUILD_CLIENT_ID="${MY_ONE_LOGIN_USER_ID:-<YOUR_SUBJECT_ID_HERE>}"
 export TABLE_NAME=user_services
+export ACTIVITY_LOG_TABLE_NAME=activity_logs
 
 aws --endpoint-url=http://localhost:4566 dynamodb create-table \
     --table-name $TABLE_NAME \
@@ -19,6 +20,23 @@ aws --endpoint-url=http://localhost:4566 dynamodb put-item \
     --region eu-west-2 \
     --item \
         "{\"user_id\":{\"S\": \"$BUILD_CLIENT_ID\"}, \"services\": {\"L\":[{\"M\":{\"count_successful_logins\":{\"N\":\"2\"},\"client_id\":{\"S\":\"gov-uk\"},\"last_accessed\":{\"N\":\"1666169856\"},\"last_accessed_pretty\":{\"S\":\"20 January 1970\"}}}]}}"
+
+aws --endpoint-url=http://localhost:4566 dynamodb create-table \
+    --table-name $ACTIVITY_LOG_TABLE_NAME \
+    --attribute-definitions AttributeName=user_id,AttributeType=S AttributeName=timestamp,AttributeType=N\
+    --key-schema AttributeName=user_id,KeyType=HASH AttributeName=timestamp,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --region eu-west-2
+
+aws --endpoint-url=http://localhost:4566 dynamodb put-item \
+    --table-name $ACTIVITY_LOG_TABLE_NAME  \
+    --region eu-west-2 \
+    --item \
+        "{\"user_id\":{\"S\": \"$BUILD_CLIENT_ID\"},
+        \"timestamp\" :{\"N\":\"1680025701\"},
+         \"session_id\" :{\"S\":\"session_123\"},
+          \"event_type\" :{\"S\":\"AUTH_AUTH_CODE_ISSUED\"},
+         \"activities\": {\"L\":[{\"M\":{\"client_id\":{\"S\":\"gov-uk\"},\"timestamp\":{\"N\":\"1666169856\"},\"type\":{\"S\":\"AUTH_AUTH_CODE_ISSUED\"}}}]}}"
 
 # Creates account-mgmt-frontend infra dependencies
 
