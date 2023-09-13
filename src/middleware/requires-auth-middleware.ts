aa5ca4f5f0a536f1aeaea0b8ee9f329592afee8d
Divya <divya.parameswaran@digital.cabinet-office.gov.uk>
2023-09-13 13:44:19 +0100

    Update console log to logger.info Include the current URL and redirect on callback if current URL is present Add test to redirect to Login and update existing tests

--- src/middleware/requires-auth-middleware.ts
+++ src/middleware/requires-auth-middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { generators } from "openid-client";
 import { PATH_DATA, VECTORS_OF_TRUST } from "../app.constants";
+import { logger } from "../utils/logger";
 
 export function requiresAuthMiddleware(
   req: Request,
@@ -10,11 +11,13 @@ export function requiresAuthMiddleware(
   const isAuthenticated = req.session.user?.isAuthenticated;
   const isLoggedOut = req.cookies?.lo;
 
-  console.log(`isAuthenticated = ${isAuthenticated} , isLoggedOut = ${isLoggedOut}`);
-
-  // if there is no session then should create a session and redirect to auth to sign in 
-  
-  if (!isAuthenticated && isLoggedOut == "true") {
+  logger.info(
+    `isAuthenticated = ${isAuthenticated} , isLoggedOut = ${isLoggedOut}`
+  );
+  // if there is no session then should create a session and redirect to auth to sign in
+  if (isAuthenticated === undefined) {
+    redirectToLogIn(req, res);
+  } else if (!isAuthenticated && isLoggedOut == "true") {
     return res.redirect(PATH_DATA.USER_SIGNED_OUT.url);
   } else if (!isAuthenticated) {
     return res.redirect(PATH_DATA.SESSION_EXPIRED.url);
@@ -24,10 +27,10 @@ export function requiresAuthMiddleware(
   }
 }
 
-async function startGet(req: Request, res: Response): Promise<void> {
+async function redirectToLogIn(req: Request, res: Response): Promise<void> {
   req.session.nonce = generators.nonce(15);
   req.session.state = generators.nonce(10);
-
+  req.session.currentURL = req.url;
   const authUrl = req.oidc.authorizationUrl({
     client_id: req.oidc.metadata.client_id,
     response_type: "code",
@@ -39,6 +42,5 @@ async function startGet(req: Request, res: Response): Promise<void> {
     vtr: JSON.stringify([VECTORS_OF_TRUST.MEDIUM]),
     _ga: req.query._ga,
   });
-
-  res.redirect(authUrl);
+  return res.redirect(authUrl);
 }
