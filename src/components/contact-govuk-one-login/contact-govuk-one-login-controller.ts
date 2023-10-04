05c43fe1f85f319875ef84332378b758b6d1ba72
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-10-04 12:06:35 +0100

    OLH-1114 extract fromUrl in request and save to session

--- src/components/contact-govuk-one-login/contact-govuk-one-login-controller.ts
+++ src/components/contact-govuk-one-login/contact-govuk-one-login-controller.ts
@@ -1,7 +1,29 @@
 import { Request, Response } from "express";
+import { isValidUrl } from "./../../utils/strings";
+import pino from "pino";
+
+const CONTACT_ONE_LOGIN_TEMPLATE = "contact-govuk-one-login/index.njk";
+const logger = pino();
 
 export function contactGet(req: Request, res: Response): void {
-  const data = {};
-
-  res.render("contact-govuk-one-login/index.njk", data);
+  const fromURL = getFromUrlAndSaveIt(req);
+  if (!fromURL) {
+    logger.info(
+      "Request to contact-govuk-one-login page did not contain a valid fromURL in the request or session"
+    );
+  }
+  const data = {
+    fromURL: fromURL,
+  };
+  res.render(CONTACT_ONE_LOGIN_TEMPLATE, data);
 }
+
+const getFromUrlAndSaveIt = (request: Request): string => {
+  const fromURLFromRequest = request.query.fromURL as string;
+  if (isValidUrl(fromURLFromRequest)) {
+    request.session.fromURL = fromURLFromRequest;
+    return fromURLFromRequest;
+  }
+  const fromURLFromSession = request.session.fromURL;
+  return fromURLFromSession;
+};
--- src/components/contact-govuk-one-login/tests/contact-govuk-one-login-controller.test.ts
+++ src/components/contact-govuk-one-login/tests/contact-govuk-one-login-controller.test.ts
@@ -1,10 +1,11 @@
 import { expect } from "chai";
 import { describe } from "mocha";
 import { Request, Response } from "express";
-
 import { sinon } from "../../../../test/utils/test-utils";
-
 import { contactGet } from "../contact-govuk-one-login-controller";
+import { logger } from "../../../utils/logger";
+
+const CONTACT_ONE_LOGIN_TEMPLATE = "contact-govuk-one-login/index.njk";
 
 describe("Contact GOV.UK One Login controller", () => {
   let sandbox: sinon.SinonSandbox;
@@ -17,6 +18,9 @@ describe("Contact GOV.UK One Login controller", () => {
     req = {
       body: {},
       cookies: { lng: "en" },
+      query: {},
+      session: {},
+      log: logger,
     };
     res = {
       render: sandbox.fake(),
@@ -35,11 +39,37 @@ describe("Contact GOV.UK One Login controller", () => {
 
   describe("contactGet", () => {
     it("should render contact centre triage page", () => {
+      const validUrl = "https://home.account.gov.uk/security";
+      req.query.fromURL = validUrl;
       contactGet(req as Request, res as Response);
+      expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
+        fromURL: validUrl,
+      });
+    });
 
-      expect(res.render).to.have.calledWith(
-        "contact-govuk-one-login/index.njk"
-      );
+    it("should render contact centre triage page when session contains fromURL", () => {
+      const validUrl = "https://home.account.gov.uk/security";
+      req.session = { fromURL: validUrl };
+      contactGet(req as Request, res as Response);
+      expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
+        fromURL: validUrl,
+      });
+    });
+
+    it("should render centre triage page when invalid fromURL is present", () => {
+      const invalidUrl = "DROP * FROM *;";
+      req.query.fromURL = invalidUrl;
+      contactGet(req as Request, res as Response);
+      expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
+        fromURL: undefined,
+      });
+    });
+
+    it("should render centre triage page when no fromURL is present", () => {
+      contactGet(req as Request, res as Response);
+      expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
+        fromURL: undefined,
+      });
     });
   });
 });
