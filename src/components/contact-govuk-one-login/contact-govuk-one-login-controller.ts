c6e66632e3f94d22aa45631999c271da958433f6
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-10-06 12:22:39 +0100

    OLH-1153 append contact centre params from mobile to session data

--- src/components/contact-govuk-one-login/contact-govuk-one-login-controller.ts
+++ src/components/contact-govuk-one-login/contact-govuk-one-login-controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import pino from "pino";
-import { isValidUrl } from "./../../utils/strings";
-<<<<<<< HEAD
+import { isSafeString, isValidUrl } from "./../../utils/strings";
 import {
   supportWebchatContact,
   supportPhoneContact,
@@ -16,33 +15,62 @@ export function contactGet(req: Request, res: Response): void {
   let isLoggedOut = req.cookies?.lo;
   const fromURL = getFromUrlAndSaveIt(req);
 
-  if (typeof(isLoggedOut) === 'string') {
-    isLoggedOut = JSON.parse(isLoggedOut)
+  if (typeof isLoggedOut === "string") {
+    isLoggedOut = JSON.parse(isLoggedOut);
   }
-  
+
   if (!fromURL) {
     logger.info(
       "Request to contact-govuk-one-login page did not contain a valid fromURL in the request or session"
     );
   }
 
+  // optional fields from mobile
+  const theme = getValueFromRequestOrSession(req, "theme");
+  const appSessionId = getValueFromRequestOrSession(req, "appSessionId");
+  const appErrorCode = getValueFromRequestOrSession(req, "appErrorCode");
+
   const data = {
     contactWebchatEnabled: supportWebchatContact(),
     contactPhoneEnabled: supportPhoneContact(),
     showContactGuidance: showContactGuidance(),
     showSignOut: isAuthenticated && !isLoggedOut,
-    fromURL
+    fromURL,
+    appSessionId,
+    appErrorCode,
+    theme,
   };
-  
-  res.render("contact-govuk-one-login/index.njk", data);
+
+  res.render(CONTACT_ONE_LOGIN_TEMPLATE, data);
 }
 
 const getFromUrlAndSaveIt = (request: Request): string => {
   const fromURLFromRequest = request.query.fromURL as string;
-  if (isValidUrl(fromURLFromRequest)) {
+  if (fromURLFromRequest && isValidUrl(fromURLFromRequest)) {
     request.session.fromURL = fromURLFromRequest;
     return fromURLFromRequest;
+  } else if (fromURLFromRequest) {
+    logger.error(
+      "fromURL in request query for contact-govuk-one-login page did not pass validation"
+    );
   }
   const fromURLFromSession = request.session.fromURL;
   return fromURLFromSession;
+};
+
+const getValueFromRequestOrSession = (
+  request: Request,
+  propertyName: string
+): string => {
+  const valueFromRequest = request.query[`${propertyName}`] as string;
+  if (valueFromRequest && isSafeString(valueFromRequest)) {
+    request.session[`${propertyName}`] = valueFromRequest;
+    return valueFromRequest;
+  } else if (valueFromRequest) {
+    logger.error(
+      `${propertyName} in request query for contact-govuk-one-login page did not pass validation`
+    );
+  }
+  const valueFromSession = request.session[`${propertyName}`];
+  return valueFromSession;
 };
--- src/components/contact-govuk-one-login/tests/contact-govuk-one-login-controller.test.ts
+++ src/components/contact-govuk-one-login/tests/contact-govuk-one-login-controller.test.ts
@@ -19,7 +19,11 @@ describe("Contact GOV.UK One Login controller", () => {
       body: {},
       cookies: { lng: "en" },
       query: {},
-      session: {},
+      session: {
+        user: {
+          isAuthenticated: true,
+        },
+      },
       log: logger,
     };
     res = {
@@ -30,6 +34,9 @@ describe("Contact GOV.UK One Login controller", () => {
     };
 
     process.env.SUPPORT_TRIAGE_PAGE = "1";
+    process.env.SUPPORT_PHONE_CONTACT = "1";
+    process.env.SHOW_CONTACT_GUIDANCE = "1";
+    process.env.SUPPORT_WEBCHAT_CONTACT = "1";
   });
 
   afterEach(() => {
@@ -42,17 +49,116 @@ describe("Contact GOV.UK One Login controller", () => {
       const validUrl = "https://home.account.gov.uk/security";
       req.query.fromURL = validUrl;
       contactGet(req as Request, res as Response);
+      // query data should be passed to the page render
       expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
+        contactWebchatEnabled: true,
+        contactPhoneEnabled: true,
+        showContactGuidance: true,
+        showSignOut: true,
         fromURL: validUrl,
+        appSessionId: undefined,
+        appErrorCode: undefined,
+        theme: undefined,
+      });
+      // query data should be saved into session
+      expect(req.session.fromURL).to.equal(validUrl);
+    });
+
+    it("should render contact centre triage page with fromURL from session and signedOut = false ", () => {
+      const validUrl = "https://home.account.gov.uk/security";
+      req.session = {
+        fromURL: validUrl,
+        user: {
+          isAuthenticated: false,
+        },
+      };
+      contactGet(req as Request, res as Response);
+      expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
+        contactWebchatEnabled: true,
+        contactPhoneEnabled: true,
+        showContactGuidance: true,
+        showSignOut: false,
+        fromURL: validUrl,
+        appSessionId: undefined,
+        appErrorCode: undefined,
+        theme: undefined,
       });
     });
 
-    it("should render contact centre triage page when session contains fromURL", () => {
+    it("should render contact centre triage page with additional fields from the mobile app", () => {
       const validUrl = "https://home.account.gov.uk/security";
-      req.session = { fromURL: validUrl };
+      const appSessionId = "123456789";
+      const appErrorCode = "ERRORCODE123";
+      const theme = "WaveyTheme";
+      req.query.fromURL = validUrl;
+      req.query.appSessionId = appSessionId;
+      req.query.appErrorCode = appErrorCode;
+      req.query.theme = theme;
+      contactGet(req as Request, res as Response);
+      // query data should be passed to the page render
+      expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
+        fromURL: validUrl,
+        appSessionId,
+        appErrorCode,
+        theme,
+        contactWebchatEnabled: true,
+        contactPhoneEnabled: true,
+        showContactGuidance: true,
+        showSignOut: true,
+      });
+      // query data should be saved into session
+      expect(req.session.fromURL).to.equal(validUrl);
+      expect(req.session.appSessionId).to.equal(appSessionId);
+      expect(req.session.appErrorCode).to.equal(appErrorCode);
+      expect(req.session.theme).to.equal(theme);
+    });
+
+    it("should render contact centre triage page with invalid fields from the mobile app", () => {
+      const validUrl = "https://home.account.gov.uk/security";
+      const appSessionId =
+        "123456789123456789123456789123456789123456789123456789123456789123456789123456789"; // too long
+      const appErrorCode = ";;***;;"; // unsafe characters
+      req.query.fromURL = validUrl;
+      req.query.appSessionId = appSessionId;
+      req.query.appErrorCode = appErrorCode;
+      contactGet(req as Request, res as Response);
+      // invalid query data not should be passed to the page render
+      expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
+        fromURL: validUrl,
+        appSessionId: undefined,
+        appErrorCode: undefined,
+        theme: undefined,
+        contactWebchatEnabled: true,
+        contactPhoneEnabled: true,
+        showContactGuidance: true,
+        showSignOut: true,
+      });
+      // invalid query data should not be saved into session
+      expect(req.session.fromURL).to.equal(validUrl);
+      expect(req.session.appSessionId).to.be.undefined;
+      expect(req.session.appErrorCode).to.be.undefined;
+      expect(req.session.theme).to.be.undefined;
+    });
+
+    it("should render contact centre triage page with additional fields from the mobile app from session", () => {
+      const validUrl = "https://home.account.gov.uk/security";
+      const appSessionId = "123456789";
+      const appErrorCode = "ERRORCODE123";
+      const theme = "WaveyTheme";
+      req.session.fromURL = validUrl;
+      req.session.appSessionId = appSessionId;
+      req.session.appErrorCode = appErrorCode;
+      req.session.theme = theme;
       contactGet(req as Request, res as Response);
       expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
         fromURL: validUrl,
+        appSessionId,
+        appErrorCode,
+        theme,
+        contactWebchatEnabled: true,
+        contactPhoneEnabled: true,
+        showContactGuidance: true,
+        showSignOut: true,
       });
     });
 
@@ -62,6 +168,13 @@ describe("Contact GOV.UK One Login controller", () => {
       contactGet(req as Request, res as Response);
       expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
         fromURL: undefined,
+        appSessionId: undefined,
+        appErrorCode: undefined,
+        theme: undefined,
+        contactWebchatEnabled: true,
+        contactPhoneEnabled: true,
+        showContactGuidance: true,
+        showSignOut: true,
       });
     });
 
@@ -69,6 +182,13 @@ describe("Contact GOV.UK One Login controller", () => {
       contactGet(req as Request, res as Response);
       expect(res.render).to.have.calledWith(CONTACT_ONE_LOGIN_TEMPLATE, {
         fromURL: undefined,
+        appSessionId: undefined,
+        appErrorCode: undefined,
+        theme: undefined,
+        contactWebchatEnabled: true,
+        contactPhoneEnabled: true,
+        showContactGuidance: true,
+        showSignOut: true,
       });
     });
   });
