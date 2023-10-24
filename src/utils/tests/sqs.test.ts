94f80d7079f6b7b6f3662ec917089d6caeb5bfa6
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2023-10-24 11:20:10 +0100

    WIP - stub SQS client

--- src/utils/tests/sqs.test.ts
+++ src/utils/tests/sqs.test.ts
@@ -1,42 +1,54 @@
 import { describe } from "mocha";
 import { sinon } from "../../../test/utils/test-utils";
-import { SqsService } from "../types";
-import { EventService } from "../../components/contact-govuk-one-login/event-service";
 import { AuditEvent } from "../../components/contact-govuk-one-login/types";
 import { expect } from "chai";
-// import { SQSClient } from "@aws-sdk/client-sqs";
-// import { sqsService } from "../../utils/sqs";
+import * as SQS from "@aws-sdk/client-sqs";
+import { sqsService } from "../../utils/sqs";
 
-describe("SQS service tests", () : void => {
-
+describe("SQS service tests", (): void => {
   let sandbox: sinon.SinonSandbox;
+  let mockSQSClient: SQS.SQSClient;
 
   beforeEach((): void => {
     sandbox = sinon.createSandbox();
+
+    const mockSQSClient = {
+      send: sandbox.fake(),
+      config: sandbox.fake(),
+      destroy: sandbox.fake(),
+      middlewareStack: sandbox.fake(),
+    };
+
+    sandbox.stub(SQS, "SQSClient").callsFake(() => {
+      return mockSQSClient;
+    });
+  });
+
+  afterEach((): void => {
+    sandbox.restore();
   });
 
   it("can send a message to an SQS queue", (): void => {
-    const fakeSqsService: SqsService = {send: sandbox.fake()};
-
-    const expectedEvent : AuditEvent = {
+    const expectedEvent: AuditEvent = {
       timestamp: undefined,
       event_name: "HOME_TRIAGE_PAGE_VISIT",
       component_id: "HOME",
       user: undefined,
       platform: undefined,
       extensions: undefined,
-    }
+    };
 
-    EventService(fakeSqsService).send(expectedEvent);
+    sqsService().send(JSON.stringify(expectedEvent));
 
-    expect(fakeSqsService.send).to.have.been.calledOnceWith(JSON.stringify(expectedEvent));
+    expect(mockSQSClient.send).to.have.been.calledOnceWith(
+      JSON.stringify(expectedEvent)
+    );
   });
 
   it("sends a failed message to the DLQ", (): void => {
     // const eventService: EventServiceInterface = EventService();
     // const stub = sinon.stub(eventService);
     // stub.send.onFirstCall().throwsException()
-
     // const fakeSqsClient: SQSClient = {send: sandbox.stub().onFirstCall().throws(new Error("AWS error"))}
     //
     // const sut: SqsService = sqsService();
@@ -50,8 +62,5 @@ describe("SQS service tests", () : void => {
     //   platform: undefined,
     //   extensions: undefined,
     // }
-
-
-
   });
 });
