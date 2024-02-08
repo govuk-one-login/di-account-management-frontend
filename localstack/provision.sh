034812c62c7bed8c6eb156399fc6fdcab1cb2ea1
Alfonso Patino <3810819+aenciso@users.noreply.github.com>
2024-02-08 17:06:10 +0000

    OLH-1299 Refactor provision to generate more items

--- localstack/provision.sh
+++ localstack/provision.sh
@@ -2,22 +2,28 @@
 
 # Creates account-mgmt-backend infra dependencies
 
+# Set the endpoint URL for DynamoDB
+ENDPOINT_URL="http://localhost:4566"
+
+# Set the AWS region
+REGION="eu-west-2"
+
 # either `export MY_ONE_LOGIN_USER_ID=xyz` otherwise the value defaults to `user_id`
 # or what ever the hardcoded replacement is
 export BUILD_CLIENT_ID="${MY_ONE_LOGIN_USER_ID:-user_id}"
 export TABLE_NAME=user_services
 export ACTIVITY_LOG_TABLE_NAME=activity_logs
 
-aws --endpoint-url=http://localhost:4566 dynamodb create-table \
+aws --endpoint-url=$ENDPOINT_URL dynamodb create-table \
     --table-name $TABLE_NAME \
     --attribute-definitions AttributeName=user_id,AttributeType=S \
     --key-schema AttributeName=user_id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
-    --region eu-west-2
+    --region $REGION
 
-aws --endpoint-url=http://localhost:4566 dynamodb put-item \
+aws --endpoint-url=$ENDPOINT_URL dynamodb put-item \
     --table-name $TABLE_NAME  \
-    --region eu-west-2 \
+    --region $REGION \
     --item '
     {
       "user_id": {
@@ -62,74 +68,45 @@ aws --endpoint-url=http://localhost:4566 dynamodb put-item \
     }'
 
 
-aws --endpoint-url=http://localhost:4566 dynamodb create-table \
+aws --endpoint-url=$ENDPOINT_URL dynamodb create-table \
     --table-name $ACTIVITY_LOG_TABLE_NAME \
     --attribute-definitions AttributeName=user_id,AttributeType=S AttributeName=timestamp,AttributeType=N\
     --key-schema AttributeName=user_id,KeyType=HASH AttributeName=timestamp,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
-    --region eu-west-2
+    --region $REGION
 
-aws --endpoint-url=http://localhost:4566 dynamodb put-item \
-    --table-name $ACTIVITY_LOG_TABLE_NAME  \
-    --region eu-west-2 \
-    --item '
-    {
-      "user_id": {
-        "S": "'"$BUILD_CLIENT_ID"'"
-      },
-      "timestamp": {
-        "N": "1680025701"
-      },
-      "session_id": {
-        "S": "session_123"
-      },
-      "client_id": {
-        "S": "vehicleOperatorLicense"
-      },
-      "event_type": {
-        "S": "AUTH_AUTH_CODE_ISSUED"
-      },
-      "event_id": {
-        "S": "4a3e09e0-ce9e-4fd5-b47e-4934f35c6e40"
-      },
-      "reported_suspicious": {
-        "BOOL": false
-      }
-    }'
+# Generate 20 activity logs
+i=1
+while [ $i -le 20 ]; do
+  # Generate a unique session_id and event_id for each item
+  SESSION_ID="session_${i}"
+  EVENT_ID=$(node -e 'var uuid=require("uuid"); console.log(uuid.v4());')
 
-aws --endpoint-url=http://localhost:4566 dynamodb put-item \
-    --table-name $ACTIVITY_LOG_TABLE_NAME  \
-    --region eu-west-2 \
-    --item '
-    {
-      "user_id": {
-        "S": "'"$BUILD_CLIENT_ID"'"
-      },
-      "timestamp": {
-        "N": "1680025800"
-      },
-      "session_id": {
-        "S": "session_123"
-      },
-      "client_id": {
-        "S": "vehicleOperatorLicense"
-      },
-      "event_type": {
-        "S": "AUTH_AUTH_CODE_ISSUED"
-      },
-      "event_id": {
-         "S": "4a3e09e0-ce9e-4fd5-b47e-4934f35c6e40"
-      },
-      "reported_suspicious": {
-         "BOOL": false
-      }
-    }'
+  # Adjust the timestamp to simulate different times
+  TIMESTAMP=$((1680025701 + i * 100))
+
+  # Use AWS CLI to put item into DynamoDB
+  aws --endpoint-url=$ENDPOINT_URL dynamodb put-item \
+      --table-name $ACTIVITY_LOG_TABLE_NAME \
+      --region $REGION \
+      --item '{
+        "user_id": {"S": "'"$BUILD_CLIENT_ID"'"},
+        "timestamp": {"N": "'$TIMESTAMP'"},
+        "session_id": {"S": "'$SESSION_ID'"},
+        "client_id": {"S": "vehicleOperatorLicense"},
+        "event_type": {"S": "AUTH_AUTH_CODE_ISSUED"},
+        "event_id": {"S": "'$EVENT_ID'"},
+        "reported_suspicious": {"BOOL": false}
+      }'
+
+  i=$((i + 1))
+done
 
 # Creates account-mgmt-frontend infra dependencies
 
-aws --endpoint-url http://localhost:4566 dynamodb create-table \
+aws --endpoint-url $ENDPOINT_URL dynamodb create-table \
   --table-name account-mgmt-frontend-SessionStore \
-  --region eu-west-2 \
+  --region $REGION \
   --attribute-definitions AttributeName=id,AttributeType=S AttributeName=user_id,AttributeType=S \
   --key-schema AttributeName=id,KeyType=HASH \
   --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
@@ -153,10 +130,10 @@ aws --endpoint-url http://localhost:4566 dynamodb create-table \
     }
   ]'
 
-aws --endpoint-url http://localhost:4566 dynamodb update-time-to-live \
+aws --endpoint-url $ENDPOINT_URL dynamodb update-time-to-live \
   --table-name account-mgmt-frontend-SessionStore \
-  --region eu-west-2 \
+  --region $REGION \
   --time-to-live-specification Enabled=true,AttributeName=expires
 
-aws sqs --endpoint-url http://localhost:4566 create-queue --queue-name audit-events \
-  --region eu-west-2
+aws sqs --endpoint-url $ENDPOINT_URL create-queue --queue-name audit-events \
+  --region $REGION
