3f8a5e507723e69d087a5b1c1867545b23e827b7
Alfonso Patino <3810819+aenciso@users.noreply.github.com>
2024-02-18 00:12:50 +0000

    OLH-1299 Update activity_log table name

--- localstack/provision.sh
+++ localstack/provision.sh
@@ -23,7 +23,7 @@ export REGION="${AWS_REGION:-us-east-1}"
 # or what ever the hardcoded replacement is
 export BUILD_CLIENT_ID="${MY_ONE_LOGIN_USER_ID:-user_id}"
 export TABLE_NAME=user_services
-export ACTIVITY_LOG_TABLE_NAME=activity_log
+export ACTIVITY_LOG_TABLE_NAME="${ACTIVITY_LOG_STORE_TABLE_NAME}"
 
 aws --endpoint-url=$ENDPOINT_URL dynamodb create-table \
     --table-name $TABLE_NAME \
