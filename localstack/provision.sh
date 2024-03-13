ded103cd32ed4cb9150a1a0cd004757fd2b88b09
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2024-03-13 11:59:33 +0000

    OLH-1580 fix for localstack upgrade.

--- localstack/provision.sh
+++ localstack/provision.sh
@@ -17,6 +17,8 @@ export ACTIVITY_LOG_TABLE_NAME=activity_log
 # Set the AWS region
 export REGION="${AWS_DEFAULT_REGION:-eu-west-2}"
 
+mkdir /tmp/keys
+
 if [ -n "$RUNNING_OUTSIDE_DOCKER" ]; then
   # Generate the first key
   OUTPUT_GENERATOR=$(aws --endpoint-url=$ENDPOINT_URL kms create-key --region $REGION)
