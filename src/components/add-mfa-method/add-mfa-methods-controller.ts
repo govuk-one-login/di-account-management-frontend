99aa731a95bc0ebb658a8ea62e8c627927528a21
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-04-08 09:12:45 +0100

    add translations

--- src/components/add-mfa-method/add-mfa-methods-controller.ts
+++ src/components/add-mfa-method/add-mfa-methods-controller.ts
@@ -2,7 +2,8 @@ import { Request, Response, NextFunction } from "express";
 import { PATH_DATA } from "../../app.constants";
 
 export function addMfaMethodGet(req: Request, res: Response): void {
-  res.render(`add-mfa-method/index.njk`);
+  const helpText = `<p>${req.t("pages.addMfaMethod.app.help.text1")}</p><p>${req.t("pages.addMfaMethod.app.help.text2")}</p>`;
+  res.render(`add-mfa-method/index.njk`, { helpText });
 }
 
 export function addMfaMethodPost(
