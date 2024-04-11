673d28f73e983bbafcf58ee45cac6f85c2efc160
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-04-11 12:25:04 +0100

    remove repeated tests

--- src/components/add-mfa-method/tests/add-mfa-methods-controller.test.ts
+++ src/components/add-mfa-method/tests/add-mfa-methods-controller.test.ts
@@ -63,58 +63,6 @@ describe("addMfaMethodPost", () => {
   });
 });
 
-describe("addMfaMethodPost", () => {
-  let sandbox: SinonSandbox;
-  let req: Partial<Request>;
-  let res: Partial<Response>;
-  let next: sinon.SinonSpy;
-
-  beforeEach(() => {
-    sandbox = sinon.createSandbox();
-    req = { body: {}, log: { error: sandbox.fake() } as any };
-    res = {
-      status: sandbox.fake(),
-      end: sandbox.fake(),
-      redirect: sandbox.fake(),
-    };
-    next = sinon.spy();
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
-
-  it("should take the use to the add backup phone number page when that option is selected", () => {
-    req.body.addMfaMethod = "sms";
-
-    addMfaMethodPost(req as Request, res as Response, next);
-
-    expect(res.redirect).to.have.been.calledWith(
-      PATH_DATA.ADD_MFA_METHOD_SMS.url
-    );
-  });
-
-  it("should take the user to the add auth app page when the user selects that option", () => {
-    req.body.addMfaMethod = "app";
-
-    addMfaMethodPost(req as Request, res as Response, next);
-
-    expect(res.redirect).to.have.been.calledWith(
-      PATH_DATA.ADD_MFA_METHOD_APP.url
-    );
-  });
-
-  it("should call next with an error when addMfaMethod is unknown", () => {
-    req.body.addMfaMethod = "unknown";
-
-    addMfaMethodPost(req as Request, res as Response, next);
-
-    const call: sinon.SinonSpyCall<Error[], unknown> = next.getCall(0);
-
-    expect(call.args[0].message).to.equal("Unknown addMfaMethod: unknown");
-  });
-});
-
 describe("addMfaAppMethodGet", () => {
   let sandbox: SinonSandbox;
 
