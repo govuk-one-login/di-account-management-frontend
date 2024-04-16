a466b971a9bc34da4425189aad94f69b8044209b
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2024-04-16 09:37:49 +0100

    OLH-1709: Log a warning if the header isn't present
    There may be times when this header isn't present - eg. if something
    goes wrong with the Cloudfront function that sets it. In those cases we
    need to be able to identify from our logs that the problem is happening
    and when it started.
    
    If this does become a frequent problem we could also set up a Cloudwatch
    metric that counts this log message and use that to trigger an alarm,
    but that's out of scope for now.

--- src/utils/test/txma-header.test.ts
+++ src/utils/test/txma-header.test.ts
@@ -1,10 +1,23 @@
 import { describe } from "mocha";
 import { expect } from "chai";
 import { Request } from "express";
+import { spy, SinonSpy } from "sinon";
 
+import { logger } from "../../utils/logger";
 import { getTxmaHeader } from "../txma-header";
 
 describe("getTxmaHeader", () => {
+  const TRACE = "trace";
+  let loggerWarnSpy: SinonSpy;
+
+  beforeEach(() => {
+    loggerWarnSpy = spy(logger, "warn");
+  });
+
+  afterEach(() => {
+    loggerWarnSpy.restore();
+  });
+
   it("returns the header value when the header is present", () => {
     const TXMA_HEADER_VALUE = "TXMA_HEADER_VALUE";
     const request: Partial<Request> = {
@@ -13,9 +26,10 @@ describe("getTxmaHeader", () => {
       },
     };
 
-    const result = getTxmaHeader(request as Request);
+    const result = getTxmaHeader(request as Request, TRACE);
 
     expect(result).to.eq(TXMA_HEADER_VALUE);
+    expect(loggerWarnSpy.notCalled).to.be.true;
   });
   it("returns an empty string when the header is not present", () => {
     const request: Partial<Request> = {
@@ -24,8 +38,9 @@ describe("getTxmaHeader", () => {
       },
     };
 
-    const result = getTxmaHeader(request as Request);
+    const result = getTxmaHeader(request as Request, TRACE);
 
     expect(result).to.eq("");
+    expect(loggerWarnSpy.calledOnce).to.be.true;
   });
 });
--- src/utils/txma-header.ts
+++ src/utils/txma-header.ts
@@ -1,5 +1,11 @@
 import { Request } from "express";
+import { logger } from "./logger";
 
-export function getTxmaHeader(req: Request): string {
-  return (req.headers["txma-audit-encoded"] as string) ?? "";
+export function getTxmaHeader(req: Request, trace: string): string {
+  if (req.headers["txma-audit-encoded"]) {
+    return req.headers["txma-audit-encoded"] as string;
+  } else {
+    logger.warn({ trace: trace }, "Missing Txma-Audit-Encoded header");
+    return "";
+  }
 }
