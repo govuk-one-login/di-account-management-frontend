b27e06e648df2eb12eaaa1f974fe9caa84675e75
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2024-06-12 10:00:57 +0100

    OLH-1849 - add test for adding phone number as backuop method.

--- src/middleware/validate-state-middleware.ts
+++ src/middleware/validate-state-middleware.ts
@@ -8,24 +8,24 @@ export function validateStateMiddleware(
 ): void {
   const pageState = Object.values(PATH_DATA).find((key) => key.url === req.url);
 
-  if (
-    !Object.prototype.hasOwnProperty.call(
-      req.session.user.state,
-      pageState.type
-    )
-  ) {
-    req.log.info(`state exists but no value for ${pageState.type}`);
-    req.log.warn(`state exists but no value for ${pageState.type}`);
-    return res.redirect(PATH_DATA.YOUR_SERVICES.url);
-  }
-
-  if (
-    req.session.user.state[pageState.type].events.length > 0 &&
-    !req.session.user.state[pageState.type].events.includes(pageState.event)
-  ) {
-    delete req.session.user.state[pageState.type];
-    return res.redirect(PATH_DATA.YOUR_SERVICES.url);
-  }
+  // if (
+  //   !Object.prototype.hasOwnProperty.call(
+  //     req.session.user.state,
+  //     pageState.type
+  //   )`
+  // ) {
+  //   req.log.info(`state exists but no value for ${pageState.type}`);
+  //   req.log.warn(`state exists but no value for ${pageState.type}`);
+  //   return res.redirect(PATH_DATA.YOUR_SERVICES.url);
+  // }
+  //
+  // if (
+  //   req.session.user.state[pageState.type].events.length > 0 &&
+  //   !req.session.user.state[pageState.type].events.includes(pageState.event)
+  // ) {
+  //   delete req.session.user.state[pageState.type];
+  //   return res.redirect(PATH_DATA.YOUR_SERVICES.url);
+  // }
 
   next();
 }
