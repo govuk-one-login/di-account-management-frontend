d35d92b485c133db105270ea24a1bed53a953364
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-07-17 11:34:48 +0100

    add await

--- src/components/delete-mfa-method/delete-mfa-method-controller.ts
+++ src/components/delete-mfa-method/delete-mfa-method-controller.ts
@@ -40,7 +40,7 @@ export async function deleteMfaMethodPost(
     return;
   }
 
-  removeMfaMethod(
+  await removeMfaMethod(
     methodToRemove.mfaIdentifier,
     await generateSessionDetails(req, res)
   );
