610aa6ea82e5a6173b0af98c41f5f92d975dcce5
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2024-07-23 18:18:59 +0100

    Amend change auth app test
    Refactor the change auth app integration test to verify that the page
    returns a 404 unless the support mfa flag is on

--- src/components/change-authenticator-app/tests/change-authenticator-app-integration.test.ts
+++ src/components/change-authenticator-app/tests/change-authenticator-app-integration.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { describe } from "mocha";
-import { sinon } from "../../../../test/utils/test-utils";
+import { sinon, expect } from "../../../../test/utils/test-utils";
 import * as cheerio from "cheerio";
 import decache from "decache";
 import { PATH_DATA } from "../../../app.constants";
@@ -10,16 +10,81 @@ import { CLIENT_SESSION_ID, SESSION_ID } from "../../../../test/utils/builders";
 import * as mfaModule from "../../../utils/mfa";
 
 describe("Integration:: change authenticator app", () => {
-  let sandbox: sinon.SinonSandbox;
   let token: string | string[];
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let cookies: string;
-  let app: any;
+  const sandbox = sinon.createSandbox();
 
-  before(async () => {
+  after(() => {
+    sandbox.restore();
+  });
+
+  it("should return change authenticator app page if feature flag is on", async () => {
+    const app = await appWithMiddlewareSetup();
+    request(app)
+      .get(PATH_DATA.CHANGE_AUTHENTICATOR_APP.url)
+      .expect((res) => {
+        const $ = cheerio.load(res.text);
+        token = $("[name=_csrf]").val();
+        cookies = res.headers["set-cookie"].concat(
+          `gs=${SESSION_ID}.${CLIENT_SESSION_ID}`
+        );
+        expect(res.status).to.equal(200);
+      });
+  });
+
+  it("should not return change authenticator app page if feature flag is off", async () => {
+    const app = await appWithMiddlewareSetup({ hideChangeMfa: true });
+
+    await request(app)
+      .get(PATH_DATA.CHANGE_AUTHENTICATOR_APP.url)
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+        token = $("[name=_csrf]").val();
+        cookies = res.headers["set-cookie"].concat(
+          `gs=${SESSION_ID}.${CLIENT_SESSION_ID}`
+        );
+        expect(res.status).to.equal(404);
+      });
+  });
+
+  it("should redirect to your services when csrf not present", async () => {
+    const app = await appWithMiddlewareSetup();
+    await checkFailedCSRFValidationBehaviour(
+      app,
+      PATH_DATA.CHANGE_AUTHENTICATOR_APP.url,
+      {
+        code: "123456",
+      }
+    );
+  });
+
+  it("should redirect to /update confirmation when valid code entered", async () => {
+    const app = await appWithMiddlewareSetup({ verifyMfaCode: true });
+
+    await request(app)
+      .post(PATH_DATA.CHANGE_AUTHENTICATOR_APP.url)
+      .type("form")
+      .send({
+        _csrf: token,
+        code: "111111",
+        authAppSecret: "qwer42312345342",
+      })
+      .then((res) => {
+        expect(
+          "Location",
+          PATH_DATA.AUTHENTICATOR_APP_UPDATED_CONFIRMATION.url
+        );
+        expect(res.status).to.equal(302);
+      });
+  });
+
+  const appWithMiddlewareSetup = async (config: any = {}) => {
     decache("../../../app");
     decache("../../../middleware/requires-auth-middleware");
     const sessionMiddleware = require("../../../middleware/requires-auth-middleware");
-    sandbox = sinon.createSandbox();
+    const sandbox = sinon.createSandbox();
+    const configFuncs = require("../../../config");
     sandbox
       .stub(sessionMiddleware, "requiresAuthMiddleware")
       .callsFake(function (req: any, res: any, next: any): void {
@@ -83,66 +148,21 @@ describe("Integration:: change authenticator app", () => {
     sandbox.replace(mfaModule, "generateMfaSecret", () => "A".repeat(20));
     sandbox.replace(mfaModule, "generateQRCodeValue", () => "qrcode");
 
-    app = await require("../../../app").createApp();
-
-    const configFuncs = require("../../../config");
+    if (config.verifyMfaCode) {
+      sandbox.replace(mfaModule, "verifyMfaCode", () => true);
+    }
 
     sandbox.stub(configFuncs, "getMfaServiceUrl").callsFake(() => {
       return "https://method-management-v1-stub.home.build.account.gov.uk";
     });
     sandbox.stub(configFuncs, "supportChangeMfa").callsFake(() => {
-      return true;
+      return !config.hideChangeMfa;
     });
+
     sandbox.stub(configFuncs, "supportMfaPage").callsFake(() => {
       return true;
     });
 
-    await request(app)
-      .get(PATH_DATA.CHANGE_AUTHENTICATOR_APP.url)
-      .then((res) => {
-        const $ = cheerio.load(res.text);
-        token = $("[name=_csrf]").val();
-        cookies = res.headers["set-cookie"].concat(
-          `gs=${SESSION_ID}.${CLIENT_SESSION_ID}`
-        );
-      });
-  });
-
-  beforeEach(() => {});
-
-  after(() => {
-    sandbox.restore();
-    app = undefined;
-  });
-
-  it("should return change authenticator app page", (done) => {
-    request(app).get(PATH_DATA.CHANGE_AUTHENTICATOR_APP.url).expect(200, done);
-  });
-
-  it("should redirect to your services when csrf not present", async () => {
-    await checkFailedCSRFValidationBehaviour(
-      app,
-      PATH_DATA.CHANGE_AUTHENTICATOR_APP.url,
-      {
-        code: "123456",
-      }
-    );
-  });
-
-  it("should redirect to /update confirmation when valid code entered", () => {
-    sandbox.replace(mfaModule, "verifyMfaCode", () => true);
-
-    // Act
-    request(app)
-      .post(PATH_DATA.CHANGE_AUTHENTICATOR_APP.url)
-      .type("form")
-      .set("Cookie", cookies)
-      .send({
-        _csrf: token,
-        code: "111111",
-        authAppSecret: "qwer42312345342",
-      })
-      .expect("Location", PATH_DATA.AUTHENTICATOR_APP_UPDATED_CONFIRMATION.url)
-      .expect(302);
-  });
+    return await require("../../../app").createApp();
+  };
 });
