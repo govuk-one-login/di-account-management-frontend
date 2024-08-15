760a414698d6bd84d777d9bb5a763893d15cf970
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-08-15 08:29:43 +0100

    add tests

--- src/components/switch-backup-method/tests/change-default-method-controller.test.ts
+++ src/components/switch-backup-method/tests/change-default-method-controller.test.ts
@@ -2,7 +2,10 @@ import { expect } from "chai";
 import { describe } from "mocha";
 import { sinon } from "../../../../test/utils/test-utils";
 
-import { switchBackupMfaMethodPost } from "../switch-backup-method-controller";
+import {
+  switchBackupMfaMethodPost,
+  switchBackupMfaMethodGet,
+} from "../switch-backup-method-controller";
 import * as mfa from "../../../utils/mfa";
 import * as mfaCommon from "../../common/mfa";
 import { UpdateInformationSessionValues } from "../../../utils/types";
@@ -14,15 +17,34 @@ describe("change default method", () => {
   const redirectFn = sinon.spy();
   const changeFn = sinon.spy();
 
-  const generateRequest = (id: string) => {
+  const generateRequest = (
+    id: string,
+    noBackup: boolean = true,
+    noDefault: boolean = true
+  ) => {
+    const mfaMethods: any[] = [];
+    if (!noBackup) {
+      mfaMethods.push({
+        mfaIdentifier: 1,
+        priorityIdentifier: "BACKUP",
+        method: {
+          mfaMethodType: "AUTH_APP",
+        },
+      });
+    }
+    if (!noDefault) {
+      mfaMethods.push({
+        mfaIdentifier: 2,
+        priorityIdentifier: "DEFAULT",
+        method: {
+          mfaMethodType: "SMS",
+          endPoint: "12345678",
+        },
+      });
+    }
     return {
       session: {
-        mfaMethods: [
-          {
-            mfaIdentifier: 1,
-            priorityIdentifier: "BACKUP",
-          },
-        ],
+        mfaMethods,
         user: {
           state: {
             switchBackupMethod: {
@@ -54,14 +76,66 @@ describe("change default method", () => {
     sandbox.restore();
   });
 
-  it("should change the DEFAULT MFA method", async () => {
-    const req = generateRequest("1");
-    const res = generateResponse();
+  describe("GET", () => {
+    it("should return a 404 if there is no backup method", async () => {
+      const req = generateRequest("1", true);
+      const res = generateResponse();
 
-    //@ts-expect-error req and res aren't valid objects since they are mocked
-    await switchBackupMfaMethodPost(req as Request, res as Response);
+      //@ts-expect-error req and res aren't valid objects since they are mocked
+      await switchBackupMfaMethodGet(req as Request, res as Response);
 
-    expect(changeFn).to.be.calledWith(1);
-    expect(redirectFn).to.be.calledWith("/switch-method-confirm");
+      expect(statusFn).to.be.calledWith(404);
+    });
+
+    it("should return a 404 if there is no default method", async () => {
+      const req = generateRequest("1", false, true);
+      const res = generateResponse();
+
+      //@ts-expect-error req and res aren't valid objects since they are mocked
+      await switchBackupMfaMethodGet(req as Request, res as Response);
+
+      expect(statusFn).to.be.calledWith(404);
+    });
+  });
+
+  describe("POST", () => {
+    it("should change the DEFAULT MFA method", async () => {
+      const req = generateRequest("1", false);
+      const res = generateResponse();
+
+      //@ts-expect-error req and res aren't valid objects since they are mocked
+      await switchBackupMfaMethodPost(req as Request, res as Response);
+
+      expect(changeFn).to.be.calledWith(1);
+      expect(redirectFn).to.be.calledWith("/switch-method-confirm");
+    });
+
+    it("should return a 404 if the new defualt method doesn't exist", async () => {
+      const req = generateRequest("5", false);
+      const res = generateResponse();
+
+      //@ts-expect-error req and res aren't valid objects since they are mocked
+      await switchBackupMfaMethodPost(req as Request, res as Response);
+
+      expect(statusFn).to.be.calledWith(404);
+    });
+
+    it("should return a 500 if there is an error when changing the defualt method", async () => {
+      sandbox.restore();
+      sandbox.replace(mfa, "changeDefaultMfaMethod", () => {
+        throw Error("error!");
+      });
+      sandbox.replace(mfaCommon, "generateSessionDetails", () => {
+        return Promise.resolve({} as UpdateInformationSessionValues);
+      });
+
+      const req = generateRequest("1", false);
+      const res = generateResponse();
+
+      //@ts-expect-error req and res aren't valid objects since they are mocked
+      await switchBackupMfaMethodPost(req as Request, res as Response);
+
+      expect(statusFn).to.be.calledWith(500);
+    });
   });
 });
