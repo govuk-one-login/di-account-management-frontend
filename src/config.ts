2213c3b6260e615155d1fa6692250b1ec2837aea
Latif Muhammad <latif.muhammad@digital.cabinet-office.gov.uk>
2024-09-30 16:00:09 +0100

    Onboarded DBT Apply for Export Certificate

--- src/config.ts
+++ src/config.ts
@@ -162,6 +162,8 @@ const COMPANY_HOUSE_ACCOUNTS_INTEGRATION = "VdmfAXiINT9wpUsGO_vVnPEbsAE";
 const COMPANY_HOUSE_ACCOUNTS_NON_PROD = "companyHouseAccounts";
 const DBT_APPLY_FOR_AN_IMPORT = "gSKx5snZtYsQWSQZRoKI2oV-7lQ";
 const DBT_APPLY_FOR_AN_IMPORT_NON_PROD = "dbtApplyForAnImportLicense";
+const DBT_APPLY_FOR_AN_EXPORT = "Xj93G5rMO2CsouiG8DJf36siQRk";
+const DBT_APPLY_FOR_AN_EXPORT_NON_PROD = "dbtApplyForAnExportCertificate";
 
 export const getAllowedAccountListClientIDs: string[] = [
   "LcueBVCnGZw-YFdTZ4S07XbQx7I",
@@ -222,6 +224,8 @@ export const getAllowedAccountListClientIDs: string[] = [
   COMPANY_HOUSE_ACCOUNTS_PROD,
   DBT_APPLY_FOR_AN_IMPORT,
   DBT_APPLY_FOR_AN_IMPORT_NON_PROD,
+  DBT_APPLY_FOR_AN_EXPORT,
+  DBT_APPLY_FOR_AN_EXPORT_NON_PROD,
 ];
 
 export const hmrcClientIds: string[] = ["mQDXGO7gWdK7V28v82nVcEGuacY", "hmrc"];
