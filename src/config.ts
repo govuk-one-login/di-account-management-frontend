38e8be6c900df035c81320e64cf3497cff0e623a
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2024-10-04 09:50:23 +0100

    Add service card for CMAD service
    Add the details for the "Confirm my apprenticeship details" service

--- src/config.ts
+++ src/config.ts
@@ -160,6 +160,8 @@ const DFE_CLAIM_ADDITIONAL_PAYMENTS_NON_PROD = "dfeClaimAdditionalPayments";
 const COMPANY_HOUSE_ACCOUNTS_PROD = "Hp9xO0Wda9EcI_2IO8OGeYJyrT0";
 const COMPANY_HOUSE_ACCOUNTS_INTEGRATION = "VdmfAXiINT9wpUsGO_vVnPEbsAE";
 const COMPANY_HOUSE_ACCOUNTS_NON_PROD = "companyHouseAccounts";
+const CMAD_PROD = "Q2tqV5C1nGXFVMUcnpqbOUTrZuw";
+const CMAD_NON_PROD = "CMAD";
 
 export const getAllowedAccountListClientIDs: string[] = [
   "LcueBVCnGZw-YFdTZ4S07XbQx7I",
@@ -218,6 +220,8 @@ export const getAllowedAccountListClientIDs: string[] = [
   COMPANY_HOUSE_ACCOUNTS_NON_PROD,
   COMPANY_HOUSE_ACCOUNTS_INTEGRATION,
   COMPANY_HOUSE_ACCOUNTS_PROD,
+  CMAD_PROD,
+  CMAD_NON_PROD,
 ];
 
 export const hmrcClientIds: string[] = ["mQDXGO7gWdK7V28v82nVcEGuacY", "hmrc"];
