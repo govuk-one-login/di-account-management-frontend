3e1ad125138eefd41b3533f1aa2e521fa8ea6962
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2024-10-16 09:17:52 +0100

    WIP

--- src/middleware/set-local-vars-middleware.ts
+++ src/middleware/set-local-vars-middleware.ts
@@ -6,8 +6,8 @@ import {
   getYourAccountUrl,
   googleAnalytics4GtmContainerId,
   universalAnalyticsGtmContainerId,
-  googleAnalytics4Disabled,
-  universalAnalyticsDisabled,
+  googleAnalytics4Enabled,
+  universalAnalyticsEnabled,
   getDtRumUrl,
 } from "../config";
 import { generateNonce } from "../utils/strings";
@@ -31,8 +31,8 @@ export function setLocalVarsMiddleware(
   res.locals.accountSignOut = PATH_DATA.SIGN_OUT.url;
   res.locals.ga4ContainerId = googleAnalytics4GtmContainerId();
   res.locals.uaContainerId = universalAnalyticsGtmContainerId();
-  res.locals.isGa4Disabled = googleAnalytics4Disabled();
-  res.locals.isUaDisabled = universalAnalyticsDisabled();
+  res.locals.isGa4Enabled = googleAnalytics4Enabled();
+  res.locals.isUaEnabled = universalAnalyticsEnabled();
   res.locals.currentUrl = getCurrentUrl(req);
 
   const sessionIds = getSessionIdsFrom(req);
