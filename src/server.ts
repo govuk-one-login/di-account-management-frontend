c177f1b90bb4fcfe8bc76edbf92e258334df6c30
Latif Muhammad <latif.muhammad@digital.cabinet-office.gov.uk>
2024-10-24 11:57:44 +0100

    installed blocked-at

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { createApp } from "./app";
 import { logger } from "./utils/logger";
+import blockedAt from "blocked-at";
 
 const port: number | string = process.env.PORT || 6001;
 
@@ -14,6 +15,29 @@ const port: number | string = process.env.PORT || 6001;
     });
   server.keepAliveTimeout = 61 * 1000;
   server.headersTimeout = 91 * 1000;
+
+  blockedAt(
+    (time, stack) => {
+      const formattedStack = (stack || [])
+        .map((frame: string) => frame.trim())
+        .join("\n");
+
+      // Log differently depending on how severe the block is
+      if (time > 200) {
+        logger.error(
+          `Severe Event loop blockage detected! blocked for ${time}ms.\nStack trace:\n${formattedStack}`
+        );
+      } else {
+        logger.warn(
+          `Event loop blocked for ${time}ms. Stack trace:\n${formattedStack}`
+        );
+      }
+    },
+    {
+      threshold: 50, // Set threshold to 50ms, feel free to adjust
+      resourcesCap: 10, // Limit to 10 stack frames to avoid too much noise
+    }
+  );
 })().catch((ex) => {
   logger.error(`Server failed to create app ${ex.message}`);
 });
