8f4e2253956c1bc019a8a21e399c510c82813b96
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-12-02 15:02:31 +0000

    add page content

--- src/components/search-services/search-services-controller.ts
+++ src/components/search-services/search-services-controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
+import { getSearchableClientsList } from "../../utils/yourServices";
+import { getAppEnv } from "../../config";
 
 const TEMPLATE_NAME = "search-services/index.njk";
 
 export function searchServicesGet(req: Request, res: Response): void {
-  res.render(TEMPLATE_NAME, {});
+  const services = getSearchableClientsList();
+  res.render(TEMPLATE_NAME, {
+    env: getAppEnv(),
+    services,
+  });
 }
--- src/components/search-services/tests/search-services-controller.test.ts
+++ src/components/search-services/tests/search-services-controller.test.ts
@@ -4,6 +4,9 @@ import { searchServicesGet } from "../search-services-controller";
 import { Request, Response } from "express";
 import { sinon } from "../../../../test/utils/test-utils";
 
+import * as services from "../../../utils/yourServices";
+import * as config from "../../../config";
+
 describe("search services controller", () => {
   let res: Partial<Response>;
   let req: Partial<Request>;
@@ -15,10 +18,20 @@ describe("search services controller", () => {
     res = {
       render: sandbox.fake(),
     };
+
+    sandbox.stub(services, "getSearchableClientsList").returns(["a", "b"]);
+    sandbox.stub(config, "getAppEnv").returns("test");
+  });
+
+  afterEach(() => {
+    sandbox.restore();
   });
 
   it("should return a hello world message", () => {
     searchServicesGet(req as Request, res as Response);
-    expect(res.render).to.have.calledWith("search-services/index.njk", {});
+    expect(res.render).to.have.calledWith("search-services/index.njk", {
+      env: "test",
+      services: ["a", "b"],
+    });
   });
 });
