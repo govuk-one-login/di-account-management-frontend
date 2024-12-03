d6b42f96e5caf26169e2db2942af7479a63cf406
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-12-03 15:55:05 +0000

    add tests

--- src/components/search-services/search-services-controller.ts
+++ src/components/search-services/search-services-controller.ts
@@ -5,9 +5,30 @@ import { getAppEnv } from "../../config";
 const TEMPLATE_NAME = "search-services/index.njk";
 
 export function searchServicesGet(req: Request, res: Response): void {
-  const services = getSearchableClientsList();
+  const query = ((req.query.q || "") as string).split(" ").map((q) => {
+    return q.toLowerCase().replace(/[^0-9a-z]/gi, "");
+  });
+  const services = getSearchableClientsList().filter((service) => {
+    if (!query) return true;
+
+    const serviceName = req
+      .t(`clientRegistry.${getAppEnv()}.${service}.header`)
+      .toLowerCase()
+      .replace(/[^0-9a-z]/gi, "");
+
+    for (const q of query) {
+      if (serviceName.includes(q)) {
+        return true;
+      }
+    }
+
+    return false;
+  });
   res.render(TEMPLATE_NAME, {
     env: getAppEnv(),
     services,
+    query: req.query.q,
+    hasSearch: !!req.query.q,
+    resultsCount: services.length,
   });
 }
