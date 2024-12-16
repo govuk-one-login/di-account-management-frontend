731f153f7d83a6b3fa88130272f2a962cb44c6ff
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2024-12-16 12:30:55 +0000

    OLH-2327 - Add additional uri to allowlist as recommended by security.

--- src/config/helmet.ts
+++ src/config/helmet.ts
@@ -103,6 +103,7 @@ export const webchatHelmetConfiguration: HelmetOptions = {
         (req: Request, res: Response): string =>
           `'${res.locals.missionLabWebSocketAddress}'`,
         "wss://*.transport.connect.eu-west-2.amazonaws.com",
+        " https://api.rollbar.com",
       ],
       workerSrc: ["blob:"],
       formAction: ["'self'", "https://*.account.gov.uk"],
