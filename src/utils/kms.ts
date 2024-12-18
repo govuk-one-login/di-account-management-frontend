78daacb7165962a2ed8e2a64a872ccec09c8c544
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2024-12-18 13:10:22 +0000

    Refactored KMS to use caching

--- src/utils/kms.ts
+++ src/utils/kms.ts
@@ -7,10 +7,27 @@ import {
 } from "@aws-sdk/client-kms";
 import { KmsService } from "./types";
 import { getKMSConfig, KmsConfig } from "../config/aws";
+import { cacheWithExpiration } from "./cache";
 
-export function kmsService(config: KmsConfig = getKMSConfig()): KmsService {
+const clientCacheDuration = 60 * 1000;
+const config: KmsConfig = getKMSConfig();
+
+const getKMSClient = async (): Promise<KMSClient> => {
+  const cacheKey = `kms-client:${config.kmsKeyId}`;
+
+  return await cacheWithExpiration<KMSClient>(
+    cacheKey,
+    () => {
+      const client = new KMSClient(config.awsConfig as any);
+      return Promise.resolve(client);
+    },
+    clientCacheDuration
+  );
+};
+
+export function kmsService(): KmsService {
   const sign = async function (payload: string): Promise<SignCommandOutput> {
-    const client = new KMSClient(config.awsConfig as any);
+    const client = await getKMSClient();
 
     const params = {
       KeyId: config.kmsKeyId,
@@ -18,11 +35,10 @@ export function kmsService(config: KmsConfig = getKMSConfig()): KmsService {
       MessageType: MessageType.RAW,
       SigningAlgorithm: SigningAlgorithmSpec.RSASSA_PKCS1_V1_5_SHA_512,
     };
+
     const request: SignCommand = new SignCommand(params);
     return await client.send(request);
   };
 
-  return {
-    sign,
-  };
+  return { sign };
 }
