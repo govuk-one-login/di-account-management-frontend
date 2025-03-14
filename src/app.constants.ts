24135dc88e7a9cfae220121e881dec1daffdcc5e
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2025-03-14 16:03:19 +0000

    OLH-2503 - Changes to support testing of retrieve endpoint in Auth MFA service.

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -201,7 +201,7 @@ export const API_ENDPOINTS = {
 };
 
 export const METHOD_MANAGEMENT_API = {
-  MFA_RETRIEVE: "/v1/mfa-methods/retrieve",
+  MFA_RETRIEVE: "/v1/mfa-methods/two-mfa",
   MFA_METHODS_ADD: "/v1/mfa-methods",
   MFA_METHODS_PUT: "/v1/mfa-methods/%s",
   MFA_METHODS_DELETE: "/v1/mfa-methods/%s",
