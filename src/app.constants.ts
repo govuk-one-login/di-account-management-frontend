97b95090671e7b3598ef6d6d052fbcd5cf83ff57
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2025-03-20 15:40:56 +0000

    OLH-2503 remove V1 from path.

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -201,10 +201,10 @@ export const API_ENDPOINTS = {
 };
 
 export const METHOD_MANAGEMENT_API = {
-  MFA_RETRIEVE: "/v1/mfa-methods/ooVD4Sm9oPoZTHdY6ekyoexHDb_AoN4uDd7uHH-eQqk",
-  MFA_METHODS_ADD: "/v1/mfa-methods",
-  MFA_METHODS_PUT: "/v1/mfa-methods/%s",
-  MFA_METHODS_DELETE: "/v1/mfa-methods/%s",
+  MFA_RETRIEVE: "/mfa-methods/ooVD4Sm9oPoZTHdY6ekyoexHDb_AoN4uDd7uHH-eQqk",
+  MFA_METHODS_ADD: "/mfa-methods",
+  MFA_METHODS_PUT: "/mfa-methods/%s",
+  MFA_METHODS_DELETE: "/mfa-methods/%s",
 };
 
 export enum NOTIFICATION_TYPE {
