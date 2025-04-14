f52195ef6cb4296490fa7421ebeb8c1f56cc89da
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-04-14 20:57:54 +0100

    OLH-2266: combine cookie consent tests into one feature

--- post-deploy-tests/tests/steps/@cookie-consent-banner.ts
+++ post-deploy-tests/tests/steps/@cookie-consent-banner.ts
@@ -7,6 +7,26 @@ Given("I visit the contact page", async ({ page }) => {
   await page.goto("/contact-gov-uk-one-login");
 });
 
+Given("I click to accept cookies", async ({ page }) => {
+  await page.getByText("Accept analytics cookies").click();
+});
+
+Then("I am shown a message confirming my acceptance", async ({ page }) => {
+  await page.getByText(
+    "You’ve accepted additional cookies. You can change your cookie settings at any time."
+  );
+});
+
+Given("I click to reject cookies", async ({ page }) => {
+  await page.getByText("Reject analytics cookies").click();
+});
+
+Then("I am shown a message confirming my rejection", async ({ page }) => {
+  await page.getByText(
+    "You’ve rejected additional cookies. You can change your cookie settings at any time."
+  );
+});
+
 Then("I can dismiss the confirmation message", async ({ page }) => {
   await page.getByRole("button", { name: "Hide cookie banner button" }).click();
   await expect(
