e03ec18051cef6ac32cbd16937043549dd87955d
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-04-15 11:02:06 +0100

    OLH-2266: rename reporting dir variable

--- post-deploy-tests/env.ts
+++ post-deploy-tests/env.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import * as v from "valibot";
 
 const envSchema = v.object({
-  TEST_REPORT_DIR_RESOLVED: v.string(),
+  TEST_REPORT_DIR: v.string(),
   TEST_ENVIRONMENT: v.union([
     v.literal("local"),
     v.literal("dev"),
@@ -14,6 +14,6 @@ const envSchema = v.object({
 
 export const env = v.parse(envSchema, {
   ...process.env,
-  TEST_REPORT_DIR_RESOLVED:
+  TEST_REPORT_DIR:
     process.env.TEST_REPORT_ABSOLUTE_DIR ?? process.env.TEST_REPORT_DIR,
 });
