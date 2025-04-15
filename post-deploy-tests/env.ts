b780ba229a0cac45e59c3f03eab2a2cfe43ca990
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-04-15 10:59:40 +0100

    OLH-2266: skip webchat tests when running against the local environment

---
+++ post-deploy-tests/env.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import * as v from "valibot";
+
+const envSchema = v.object({
+  TEST_REPORT_DIR_RESOLVED: v.string(),
+  TEST_ENVIRONMENT: v.union([
+    v.literal("local"),
+    v.literal("dev"),
+    v.literal("build"),
+    v.literal("staging"),
+    v.literal("production"),
+  ]),
+});
+
+export const env = v.parse(envSchema, {
+  ...process.env,
+  TEST_REPORT_DIR_RESOLVED:
+    process.env.TEST_REPORT_ABSOLUTE_DIR ?? process.env.TEST_REPORT_DIR,
+});
