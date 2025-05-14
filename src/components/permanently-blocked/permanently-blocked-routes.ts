71d5ea3131bb86ce035e8682c840f219a4d96e9a
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-05-14 15:22:19 +0100

    add global try catch to router

--- src/components/permanently-blocked/permanently-blocked-routes.ts
+++ src/components/permanently-blocked/permanently-blocked-routes.ts
@@ -2,13 +2,14 @@ import { PATH_DATA } from "../../app.constants";
 import * as express from "express";
 import { requiresAuthMiddleware } from "../../middleware/requires-auth-middleware";
 import { permanentlyBlockedGet } from "./permanently-blocked-controller";
+import { globalTryCatch } from "src/utils/global-try-catch";
 
 const router = express.Router();
 
 router.get(
   PATH_DATA.UNAVAILABLE_PERMANENT.url,
   requiresAuthMiddleware,
-  permanentlyBlockedGet
+  globalTryCatch(permanentlyBlockedGet)
 );
 
 export { router as permanentlyBlockedRouter };
--- src/components/temporarily-blocked/temporarily-blocked-routes.ts
+++ src/components/temporarily-blocked/temporarily-blocked-routes.ts
@@ -2,12 +2,13 @@ import { PATH_DATA } from "../../app.constants";
 import * as express from "express";
 import { requiresAuthMiddleware } from "../../middleware/requires-auth-middleware";
 import { temporarilyBlockedGet } from "./temporarily-blocked-controller";
+import { globalTryCatch } from "src/utils/global-try-catch";
 const router = express.Router();
 
 router.get(
   PATH_DATA.UNAVAILABLE_TEMPORARY.url,
   requiresAuthMiddleware,
-  temporarilyBlockedGet
+  globalTryCatch(temporarilyBlockedGet)
 );
 
 export { router as temporarilyBlockedRouter };
