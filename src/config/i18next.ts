4d9e1457fa9e0028a412e0b6f1a9aabc53d5f65c
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-06-09 09:49:21 +0100

    OLH-2721: Lock fallback language to EN

--- src/config/i18next.ts
+++ src/config/i18next.ts
@@ -1,11 +1,11 @@
-import { ENVIRONMENT_NAME, LOCALE } from "../app.constants";
-import { getNodeEnv, getServiceDomain } from "../config";
+import { LOCALE } from "../app.constants";
+import { getServiceDomain } from "../config";
 import type { InitOptions } from "i18next/typescript/options";
 
 export function i18nextConfigurationOptions(): InitOptions {
   return {
     debug: false,
-    fallbackLng: getNodeEnv() === ENVIRONMENT_NAME.TEST ? LOCALE.EN : "",
+    fallbackLng: LOCALE.EN,
     preload: [LOCALE.EN],
     supportedLngs: [LOCALE.EN, LOCALE.CY],
     detection: {
