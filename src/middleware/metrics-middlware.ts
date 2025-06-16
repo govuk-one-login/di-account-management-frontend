c87028b73b7ef87706f51e481b4fb66ffc75200e
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-06-16 15:40:20 +0100

    OLH-2329: migrate to cw put metric

--- src/middleware/metrics-middlware.ts
+++ src/middleware/metrics-middlware.ts
@@ -1,10 +1,33 @@
 import { NextFunction, Request, Response } from "express";
-import { Metrics, MetricUnit } from "@aws-lambda-powertools/metrics";
+import {
+  PutMetricDataCommand,
+  StandardUnit,
+  MetricDatum,
+  CloudWatchClient,
+} from "@aws-sdk/client-cloudwatch";
 import { logger } from "../utils/logger";
+
+export const cloudWatchClient = new CloudWatchClient({});
+
+interface Metric {
+  MetricName: string;
+  Dimensions?: { Name: string; Value: string }[];
+  Unit: StandardUnit;
+  Value: number;
+  Timestamp?: Date;
+}
 
 declare module "express-serve-static-core" {
   interface Request {
-    metrics?: Metrics;
+    metrics?: {
+      addMetric: (
+        name: string,
+        unit: StandardUnit,
+        value: number,
+        dimensions?: { Name: string; Value: string }[],
+        timestamp?: Date
+      ) => void;
+    };
   }
 }
 
@@ -13,24 +36,47 @@ export function metricsMiddleware(
   serviceName = "Frontend"
 ) {
   return (req: Request, res: Response, next: NextFunction) => {
-    const metrics = new Metrics({
-      namespace,
-      serviceName,
-    });
+    const customMetrics: Metric[] = [];
+    req.metrics = {
+      addMetric: (
+        name: string,
+        unit: StandardUnit,
+        value: number,
+        dimensions?: { Name: string; Value: string }[],
+        timestamp?: Date
+      ) => {
+        customMetrics.push({
+          MetricName: name,
+          Unit: unit,
+          Value: value,
+          Dimensions: [
+            ...(dimensions || []),
+            { Name: "ServiceName", Value: serviceName },
+          ],
+          Timestamp: timestamp || new Date(),
+        });
+      },
+    };
 
-    req.metrics = metrics;
-
-    res.on("finish", async () => {
-      const statusCode = res.statusCode;
-      metrics.addMetric(`HttpStatus_${statusCode}`, MetricUnit.Count, 1);
-
-      try {
-        metrics.publishStoredMetrics();
-      } catch (error) {
-        logger.error("Failed to publish metric", error);
+    res.on("finish", () => {
+      req.metrics?.addMetric(
+        `HttpStatus_${res.statusCode}`,
+        StandardUnit.Count,
+        1
+      );
+      for (let i = 0; i < customMetrics.length; i += 20) {
+        const batch = customMetrics.slice(i, i + 20);
+        const params = {
+          Namespace: namespace,
+          MetricData: batch as MetricDatum[],
+        };
+        try {
+          cloudWatchClient.send(new PutMetricDataCommand(params));
+        } catch (error) {
+          logger.error("Failed to publish metric batch", error);
+        }
       }
     });
-
     next();
   };
 }
