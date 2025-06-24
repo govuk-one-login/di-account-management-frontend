e26c93a000ec43f96f056386d84df618b0e907b3
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2025-06-24 09:18:14 +0100

    OLH-2718 Add resource frontend for deployment as well

--- deploy/dev-platform-stack.tf
+++ deploy/dev-platform-stack.tf
@@ -73,28 +73,32 @@ resource "aws_cloudformation_stack" "frontend_stack" {
   name          = "devplatform-cloudfront"
   template_body = data.local_file.frontend_cf_template.content
   parameters = {
-    AddWWWPrefix = "false"
-    CloudFrontWafACL = "none"
-    DistributionAlias = "home.dev.account.gov.uk"
-    EnableCustomErrorPages = "true"
-    FraudHeaderEnabled = "true"
-    FraudHeadersFunctionName = "TICFFraudHeadersCloudFrontFunction"
-    LogDestination = "csls_cw_logs_destination_prodpython"
-    OriginCloakingHeader = "none"
-    OriginCloakingHeaderManagedSecretAlarmSNSTopicARN = "arn:aws:sns:eu-west-2:985326104449:platform-alerting-BuildNotificationTopic"
-    OriginCloakingHeaderManagedSecretNotificationSNSTopicARN = "arn:aws:sns:eu-west-2:985326104449:platform-alerting-BuildNotificationTopic"
-    OriginCloakingHeaderManagedSecretNotificationSNSTopicKMSKeyARN = "arn:aws:kms:eu-west-2:985326104449:key/337a80b6-5117-4c16-81b6-c017d7283605"
-    OriginCloakingHeaderManagedSecretPreviousVersion = "AWSPREVIOUS"
-    OriginCloakingHeaderManagedSecretRotationMonthWeekDaySchedule = "THU#3"
-    OriginCloakingHeaderManagedSecretVersion = "AWSCURRENT"
-    PreviousOriginCloakingHeader = "none"
-    StandardLoggingEnabled = "true"
-    CloudFrontCertArn = aws_cloudformation_stack.certificate_stack.outputs["CertificateARN"]
+    ActivityLogTableName = "activity_log"
+    AlertingStackName = "platform-alerting"
+    BackendStackName = "account-mgmt-backend"
+    CodeSigningConfigArn = "arn:aws:lambda:eu-west-2:985326104449:code-signing-config:csc-0426ad10dd582a7d7"
+    ContainerPort = "6001"
+    DeploymentStrategy = "CodeDeployDefault.ECSAllAtOnce"
+    ELB4XXAlarmMetric = "HTTPCode_Target_4XX_Count"
+    Environment = "dev"
+    NotificationTopicArn = "arn:aws:sns:eu-west-2:985326104449:platform-alerting-BuildNotificationTopic"
+    OwnerTagValue = "govuk-accounts-tech@digital.cabinet-office.gov.uk"
+    PermissionsBoundary = "arn:aws:iam::985326104449:policy/account-mgmt-frontend-pipeline-AppPermissionsBoundary-0a6cc7ce271a"
+    ProductTagValue = "GOV.UK Sign In"
+    ServiceStoreTableName = "user_services"
+    SourceTagValue = "govuk-one-login/di-account-management-frontend/deploy/template.yaml"
+    StubsStackName = "account-mgmt-stubs"
+    SystemTagValue = "Accounts"
+    VpcStackName = "vpc-enhanced"
   }
 
   tags = {
-    FMSGlobalCustomPolicy = "true"
-    FMSGlobalCustomPolicyName = "cloudfront"
+    Environment = "dev"
+    is-pipeline-deployment = "False"
+    Owner = "govuk-accounts-tech@digital.cabinet-office.gov.uk"
+    Product = "GOV.UK Sign In"
+    repository = "govuk-one-login/di-account-management-frontend"
+    System = "Accounts"
   }
 
   capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
