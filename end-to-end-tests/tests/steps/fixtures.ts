fca663169d11215a5279b69803d37d2fd6d7dcf1
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-06-27 15:34:02 +0100

    OLH-2819: initial work on running end to end tests in CI

--- end-to-end-tests/tests/steps/fixtures.ts
+++ end-to-end-tests/tests/steps/fixtures.ts
@@ -13,15 +13,16 @@ export const test = base.extend<{
   nock: async ({}, use) => {
     if (!nock.isActive()) nock.activate();
 
-    const sdfsdfs =
-      "^https://method-management-v1-stub.home.dev.account.gov.uk/v1|^https://a3bnrbtiga-vpce-0c9ce65be09f99db7.execute-api.eu-west-2.amazonaws.com/staging/v1|^https://z7lornzyy5-vpce-0e594accb3d775457.execute-api.eu-west-2.amazonaws.com/integration/v1|^https://63qq2dsjo5-vpce-0d7972874707185a0.execute-api.eu-west-2.amazonaws.com/production/v1";
-
     await use({
       nock,
-      methodManagementApi: nock(new RegExp(sdfsdfs)),
+      methodManagementApi: nock(
+        new RegExp(
+          `^${RegExp.escape("https://method-management-v1-stub.home.dev.account.gov.uk/v1")}|^${RegExp.escape("https://a3bnrbtiga-vpce-0c9ce65be09f99db7.execute-api.eu-west-2.amazonaws.com/staging/v1")}|^${RegExp.escape("https://z7lornzyy5-vpce-0e594accb3d775457.execute-api.eu-west-2.amazonaws.com/integration/v1")}|^${RegExp.escape("https://63qq2dsjo5-vpce-0d7972874707185a0.execute-api.eu-west-2.amazonaws.com/production/v1")}`
+        )
+      ),
       accountManagementApi: nock(
         new RegExp(
-          "^https://am-stub.home.dev.account.gov.uk|^https://manage.staging.account.gov.uk|^https://manage.build.account.gov.uk|^https://manage.integration.account.gov.uk|^https://manage.account.gov.uk"
+          `^${RegExp.escape("https://am-stub.home.dev.account.gov.uk")}|^${RegExp.escape("https://manage.staging.account.gov.uk")}|^${RegExp.escape("https://manage.build.account.gov.uk")}|^${RegExp.escape("https://manage.integration.account.gov.uk")}|^${RegExp.escape("https://manage.account.gov.uk")}`
         )
       ),
     });
