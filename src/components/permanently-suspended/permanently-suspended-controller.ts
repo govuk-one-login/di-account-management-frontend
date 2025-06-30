096b5b62bd538828491fd9668242965c18004e93
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2025-06-30 09:38:14 +0100

    OLH-2821 Use the service search link when the feature flag is enabled

--- src/components/permanently-suspended/permanently-suspended-controller.ts
+++ src/components/permanently-suspended/permanently-suspended-controller.ts
@@ -1,6 +1,9 @@
 import type { Request, Response } from "express";
+import { supportSearchableList } from "../../config";
 
 export function permanentlySuspendedGet(req: Request, res: Response): void {
   res.status(401);
-  res.render("permanently-suspended/index.njk");
+  res.render("permanently-suspended/index.njk", {
+    searchableListEnabled: supportSearchableList(),
+  });
 }
