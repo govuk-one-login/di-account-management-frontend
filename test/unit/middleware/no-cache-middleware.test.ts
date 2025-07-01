68adbfd2350892ad44e0af7ed55f29722c2f39c6
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2025-07-01 09:17:50 +0100

    BAU: Add test for no cache middleware

---
+++ test/unit/middleware/no-cache-middleware.test.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { expect, sinon } from "../../utils/test-utils";
+import { describe } from "mocha";
+import { noCacheMiddleware } from "../../../src/middleware/no-cache-middleware";
+
+describe("no-cache-middleware", () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+
+  beforeEach(() => {
+    req = {} as Partial<Request>;
+    res = {
+      set: sinon.fake(),
+    };
+    next = sinon.fake(() => {});
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe("noCacheMiddleware", () => {
+    it("should set Cache-Control header with no-cache directives", () => {
+      noCacheMiddleware(req as Request, res as Response, next);
+
+      expect(res.set).to.have.been.calledOnceWith(
+        "Cache-Control",
+        "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+      );
+      expect(next).to.have.been.calledOnce;
+    });
+  });
+});
