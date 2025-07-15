e7eb7007f3759dd6f356f67af836088a861b2079
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-07-15 09:37:20 +0100

    OLH-2921: search improvements

--- src/components/search-services/search-services-controller.ts
+++ src/components/search-services/search-services-controller.ts
@@ -2,40 +2,42 @@ import { Request, Response } from "express";
 import { getAppEnv, getClientsToShowInSearch } from "../../config";
 import { LOCALE } from "../../app.constants";
 import { MetricUnit } from "@aws-lambda-powertools/metrics";
+import Fuse from "fuse.js/basic";
 
 const TEMPLATE_NAME = "search-services/index.njk";
 
-const prepareForSearch = (q: string): string => {
-  return q.toLowerCase().replace(/[^0-9a-z]/gi, "");
-};
-
 export function searchServicesGet(req: Request, res: Response): void {
   req.metrics?.addMetric("searchServicesGet", MetricUnit.Count, 1);
-  const query = ((req.query.q || "") as string)
-    .split(" ")
-    .map(prepareForSearch);
+  const query = (req.query.q ?? "") as string;
+  const locale = req.language ?? LOCALE.EN;
 
-  const services = getClientsToShowInSearch(req.language ?? LOCALE.EN)
-    .filter((service) => {
-      if (query.length === 0) return true;
-
-      const serviceName = prepareForSearch(
-        req.t(`clientRegistry.${getAppEnv()}.${service}.startText`)
-      );
-
-      for (const q of query) {
-        if (serviceName.includes(q)) {
-          return true;
-        }
-      }
-
-      return false;
-    })
+  let services = getClientsToShowInSearch(locale)
+    .map((service) => ({
+      startUrl: req.t(`clientRegistry.${getAppEnv()}.${service}.startUrl`),
+      startText: req.t(`clientRegistry.${getAppEnv()}.${service}.startText`),
+      additionalSearchTerms:
+        req.t(
+          `clientRegistry.${getAppEnv()}.${service}.additionalSearchTerms`
+        ) === `clientRegistry.${getAppEnv()}.${service}.additionalSearchTerms`
+          ? ""
+          : req.t(
+              `clientRegistry.${getAppEnv()}.${service}.additionalSearchTerms`
+            ),
+    }))
     .sort((a, b) => {
-      const a_trn = req.t(`clientRegistry.${getAppEnv()}.${a}.startText`);
-      const b_trn = req.t(`clientRegistry.${getAppEnv()}.${b}.startText`);
-      return a_trn.localeCompare(b_trn, req.language ?? LOCALE.EN);
+      return a.startText.localeCompare(b.startText, locale);
     });
+
+  if (query.trim().length) {
+    const fuse = new Fuse(services, {
+      keys: ["startText", "additionalSearchTerms"],
+      findAllMatches: true,
+    });
+
+    services = fuse.search(query).map((service: any) => ({
+      ...service.item,
+    }));
+  }
 
   const url = new URL(req.originalUrl, "http://example.com");
   url.searchParams.set("lng", LOCALE.EN);
@@ -45,7 +47,7 @@ export function searchServicesGet(req: Request, res: Response): void {
   res.render(TEMPLATE_NAME, {
     env: getAppEnv(),
     services,
-    query: req.query.q,
+    query,
     hasSearch: !!req.query.q,
     resultsCount: services.length,
     isWelsh: req.language === LOCALE.CY,
