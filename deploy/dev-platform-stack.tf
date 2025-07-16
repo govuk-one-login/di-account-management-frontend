74f3ec037a99d04ea747261e990784d91b4d37e0
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2025-07-16 08:17:46 +0100

    OLH-2718: Format Terraform
    Using `terraform fmt`

--- deploy/dev-platform-stack.tf
+++ deploy/dev-platform-stack.tf
@@ -1,45 +1,45 @@
 resource "aws_cloudformation_stack" "certificate_stack" {
-  provider      = aws.virginia
-  name          = "devplatform-certificate"
+  provider     = aws.virginia
+  name         = "devplatform-certificate"
   template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/certificate/template.yaml"
 
   parameters = {
-    AddWWWPrefix = "false"
+    AddWWWPrefix       = "false"
     AlternativeNameOne = var.certAlternativeNameOne
-    DomainName = var.certDomainName
-    HostedZoneID = var.certHostedZoneID
+    DomainName         = var.certDomainName
+    HostedZoneID       = var.certHostedZoneID
   }
 
   capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
 }
 
 resource "aws_cloudformation_stack" "cloudfront_stack" {
-  provider      = aws.london
-  name          = "devplatform-cloudfront"
+  provider     = aws.london
+  name         = "devplatform-cloudfront"
   template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/cloudfront-distribution/template.yaml"
 
   parameters = {
-    AddWWWPrefix = "false"
-    CloudFrontWafACL = "none"
-    DistributionAlias = var.cloudfrontDistributionAlias
-    EnableCustomErrorPages = "true"
-    FraudHeaderEnabled = "true"
-    FraudHeadersFunctionName = "TICFFraudHeadersCloudFrontFunction"
-    LogDestination = "csls_cw_logs_destination_prodpython"
-    OriginCloakingHeader = "none"
-    OriginCloakingHeaderManagedSecretAlarmSNSTopicARN = var.originCloakingHeaderManagedSecretAlarmSNSTopicARN
-    OriginCloakingHeaderManagedSecretNotificationSNSTopicARN = var.originCloakingHeaderManagedSecretNotificationSNSTopicARN
+    AddWWWPrefix                                                   = "false"
+    CloudFrontWafACL                                               = "none"
+    DistributionAlias                                              = var.cloudfrontDistributionAlias
+    EnableCustomErrorPages                                         = "true"
+    FraudHeaderEnabled                                             = "true"
+    FraudHeadersFunctionName                                       = "TICFFraudHeadersCloudFrontFunction"
+    LogDestination                                                 = "csls_cw_logs_destination_prodpython"
+    OriginCloakingHeader                                           = "none"
+    OriginCloakingHeaderManagedSecretAlarmSNSTopicARN              = var.originCloakingHeaderManagedSecretAlarmSNSTopicARN
+    OriginCloakingHeaderManagedSecretNotificationSNSTopicARN       = var.originCloakingHeaderManagedSecretNotificationSNSTopicARN
     OriginCloakingHeaderManagedSecretNotificationSNSTopicKMSKeyARN = var.originCloakingHeaderManagedSecretNotificationSNSTopicKMSKeyARN
-    OriginCloakingHeaderManagedSecretPreviousVersion = "AWSPREVIOUS"
-    OriginCloakingHeaderManagedSecretRotationMonthWeekDaySchedule = "THU#3"
-    OriginCloakingHeaderManagedSecretVersion = "AWSCURRENT"
-    PreviousOriginCloakingHeader = "none"
-    StandardLoggingEnabled = "true"
-    CloudFrontCertArn = aws_cloudformation_stack.certificate_stack.outputs["CertificateARN"]
+    OriginCloakingHeaderManagedSecretPreviousVersion               = "AWSPREVIOUS"
+    OriginCloakingHeaderManagedSecretRotationMonthWeekDaySchedule  = "THU#3"
+    OriginCloakingHeaderManagedSecretVersion                       = "AWSCURRENT"
+    PreviousOriginCloakingHeader                                   = "none"
+    StandardLoggingEnabled                                         = "true"
+    CloudFrontCertArn                                              = aws_cloudformation_stack.certificate_stack.outputs["CertificateARN"]
   }
 
   tags = {
-    FMSGlobalCustomPolicy = "true"
+    FMSGlobalCustomPolicy     = "true"
     FMSGlobalCustomPolicyName = "cloudfront"
   }
 
