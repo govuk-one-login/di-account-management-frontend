c70c65dc4eb1c16b368b9606fc7441d831bc4958
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-07-21 14:32:34 +0100

    OLH-2883: improved handling of switch MFA methods errors

--- src/middleware/log-error-middleware.ts
+++ src/middleware/log-error-middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { shouldLogError } from "../utils/shouldLogError";
+import { MetricUnit } from "@aws-lambda-powertools/metrics";
 
 export function logErrorMiddleware(
   error: any,
@@ -7,7 +8,15 @@ export function logErrorMiddleware(
   res: Response,
   next: NextFunction
 ): void {
+  req.metrics?.addMetric("logErrorMiddleware", MetricUnit.Count, 1);
+
   if (shouldLogError(error)) {
+    req.metrics?.addMetric(
+      "logErrorMiddlewareErrorLogged",
+      MetricUnit.Count,
+      1
+    );
+
     req.log.error(error, error?.message);
   }
   next(error);
