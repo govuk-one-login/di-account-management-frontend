5440dedb003cfffac29d92e6bd79a83fb3a6f300
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-08-06 16:30:39 +0100

    OLH-2955: fix session redirect race condition

--- src/app.ts
+++ src/app.ts
@@ -96,6 +96,7 @@ const APP_VIEWS = [
 
 async function createApp(): Promise<express.Application> {
   const app: express.Application = express();
+
   const isDeployedEnvironment = !isLocalEnv();
   app.use(metricsMiddleware());
   app.enable("trust proxy");
@@ -153,6 +154,20 @@ async function createApp(): Promise<express.Application> {
       ),
     })
   );
+
+  app.use((req, res, next) => {
+    // Here we monkey-patch res.redirect to defer calling it until after the session has been saved.
+    // This prevents race conditions where the user follows a redirect response and the
+    // subsequent request reads session before the changes to the session made in the previous
+    // request have been saved.
+    const originalRedirect = res.redirect;
+    res.redirect = (...args: [number, string] | [string]) => {
+      req.session.save(() => {
+        originalRedirect.call(res, ...args);
+      });
+    };
+    next();
+  });
 
   app.locals.sessionStore = sessionStore;
 
