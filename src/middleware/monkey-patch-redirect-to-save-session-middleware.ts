00e91f34e3fb6b6c46bb0a488e587d53054291a4
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-08-18 16:39:08 +0100

    OLH-3076: redirect monkey patch improvements

--- src/middleware/monkey-patch-redirect-to-save-session-middleware.ts
+++ src/middleware/monkey-patch-redirect-to-save-session-middleware.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { logger } from "../utils/logger";
 
 export function monkeyPatchRedirectToSaveSessionMiddleware(
   req: Request,
@@ -10,13 +11,27 @@ export function monkeyPatchRedirectToSaveSessionMiddleware(
   // subsequent request reads session before the changes to the session made in the previous
   // request have been saved.
   const originalRedirect = res.redirect;
+
   res.redirect = (...args: [number, string] | [string]) => {
+    const stack = new Error().stack;
+
+    const redirect = () => {
+      if (!res.headersSent) {
+        originalRedirect.call(res, ...args);
+      } else {
+        logger.warn(
+          { trace: res?.locals?.trace, stack, path: req.path },
+          "Unable to redirect as headers are already sent"
+        );
+      }
+    };
+
     if (req.session) {
       req.session.save(() => {
-        originalRedirect.call(res, ...args);
+        redirect();
       });
     } else {
-      originalRedirect.call(res, ...args);
+      redirect();
     }
   };
   next();
