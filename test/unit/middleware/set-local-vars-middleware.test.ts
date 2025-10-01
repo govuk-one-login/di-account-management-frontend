1c968664a8cd272e6fe8c6feff2af5fbc3ae7e12
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2025-10-01 13:28:24 +0100

    OLH-767 stub static hash module in local vars test

--- test/unit/middleware/set-local-vars-middleware.test.ts
+++ test/unit/middleware/set-local-vars-middleware.test.ts
@@ -34,6 +34,9 @@ describe("set-local-vars-middleware", () => {
       redirect: sandbox.fake(() => {}),
     };
     next = sandbox.fake(() => {});
+    sandbox
+      .stub(hashModule, "generateStaticHash")
+      .resolves("static-hash-12345");
   });
 
   afterEach(() => {
