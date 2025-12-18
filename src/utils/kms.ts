0f50173b5162fe3c16dc6710a7cdf5aa6ef82375
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2025-12-18 12:41:51 +0000

    OLH-3247: Request object tweak for authorize endpoint
    Tweak request to the authorize endpoint to include a signed request
    object. This is an additional step to improve security and provide
    additional assurance that the request is actually originating from the
    Home RP.
    Introduce this behind a feature flag for the time being.

--- src/utils/kms.ts
+++ src/utils/kms.ts
@@ -10,14 +10,14 @@ import { getKMSConfig, KmsConfig } from "../config/aws";
 
 const config: KmsConfig = getKMSConfig();
 const client = new KMSClient(config.awsConfig as any);
-
+export const signingAlgorithm = SigningAlgorithmSpec.RSASSA_PKCS1_V1_5_SHA_512;
 export const kmsService: KmsService = {
   sign: async (payload: string): Promise<SignCommandOutput> => {
     const params = {
       KeyId: config.kmsKeyId,
       Message: Buffer.from(payload),
       MessageType: MessageType.RAW,
-      SigningAlgorithm: SigningAlgorithmSpec.RSASSA_PKCS1_V1_5_SHA_512,
+      SigningAlgorithm: signingAlgorithm,
     };
     const request: SignCommand = new SignCommand(params);
     return await client.send(request);
