54bd114c35a7a5f16dd1b54bff558178641741a3
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2026-01-07 17:41:20 +0000

    OLH-3434: reuse redirectToLogin for start controller

--- src/components/start/start-controller.ts
+++ src/components/start/start-controller.ts
@@ -1,26 +1,8 @@
 import { Request, Response } from "express";
-import { generators } from "openid-client";
-import { VECTORS_OF_TRUST } from "../../app.constants";
 import { MetricUnit } from "@aws-lambda-powertools/metrics";
+import { redirectToLogIn } from "../../middleware/requires-auth-middleware";
 
 export async function startGet(req: Request, res: Response): Promise<void> {
   req.metrics?.addMetric("signedOutGet", MetricUnit.Count, 1);
-  req.session.nonce = generators.nonce(15);
-  req.session.state = generators.nonce(10);
-
-  const authUrl = req.oidc.authorizationUrl({
-    client_id: req.oidc.metadata.client_id,
-    response_type: "code",
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    scope: req.oidc.metadata.scopes,
-    state: req.session.state,
-    nonce: req.session.nonce,
-    redirect_uri: req.oidc.metadata.redirect_uris[0],
-    cookie_consent: req.query.cookie_consent,
-    vtr: JSON.stringify([VECTORS_OF_TRUST.MEDIUM]),
-    _ga: req.query._ga,
-  });
-
-  res.redirect(authUrl);
+  await redirectToLogIn(req, res);
 }
