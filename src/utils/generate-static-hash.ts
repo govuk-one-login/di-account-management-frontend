70594eddd815a5c62770859b0385c2c0a98d9ae7
Martin Gwarada <martin.gwarada@digital.cabinet-office.gov.uk>
2026-02-03 11:46:31 +0000

    OLH-755: added static-hash.json as it cannot find file

--- src/utils/generate-static-hash.ts
+++ src/utils/generate-static-hash.ts
@@ -1,5 +1,5 @@
-import { hash } from "./static-hash.js";
+import staticHash from "./static-hash.json" with { type: "json" };
 
 export const generateStaticHash = async (): Promise<string> => {
-  return hash;
+  return staticHash.hash;
 };
