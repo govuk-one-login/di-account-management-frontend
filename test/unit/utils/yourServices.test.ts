86f11163781e3de4b68b22b258c82d8000db9b9e
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-02-06 15:20:02 +0000

    OLH-3768: update commit

--- test/unit/utils/yourServices.test.ts
+++ test/unit/utils/yourServices.test.ts
@@ -143,7 +143,7 @@ describe("YourService Util", () => {
           last_accessed: 1696969856,
           client_id: "hmrc",
           last_accessed_pretty: "20 January 1970",
-          isAvailableInWelsh: false,
+          isAvailableInWelsh: true,
         },
       ]);
     });
