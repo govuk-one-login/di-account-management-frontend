9854774091081282033296c9d40fa9720f08e4d0
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-03-04 14:47:02 +0000

    OLH-3865: move AMC_CALLBACK path

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -5,7 +5,6 @@ export const PATH_DATA: Record<
   string,
   { url: string; event?: EventType; type?: UserJourney }
 > = {
-  AMC_CALLBACK: { url: "/amc/callback" },
   CONTACT: { url: "/contact-gov-uk-one-login" },
   SIGN_IN_HISTORY: { url: "/activity-history" },
   REPORT_SUSPICIOUS_ACTIVITY: { url: "/activity-history/report-activity" },
@@ -200,6 +199,7 @@ export const PATH_DATA: Record<
   LOGOUT_REDIRECT: {
     url: "/logout-redirect",
   },
+  AMC_CALLBACK: { url: "/amc/callback" },
 };
 
 export const EXTERNAL_URLS = {
