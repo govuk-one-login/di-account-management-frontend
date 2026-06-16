66358efa45eacd26cc44be58cbfa12b2450acea9
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-06-16 22:08:21 +0100

    OLH-4365: dedupe code

--- src/middleware/block-passkeys-routes-if-not-enabled.ts
+++ src/middleware/block-passkeys-routes-if-not-enabled.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import { passkeysEnabled } from "../config.js";
-import { mfaMethodMiddleware } from "./mfa-method-middleware.js";
 
-export const blockPasskeyRoutesIfNotEnabled = async (
+export const blockPasskeyRoutesIfNotEnabled = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (!passkeysEnabled(req)) {
-    await mfaMethodMiddleware(req, res, next);
-  } else {
+  if (passkeysEnabled(req)) {
     next();
+    return;
   }
+  res.status(404);
+  res.send();
 };
